6be4eb9b76f5fdd7b3c2f985f16790af3423ac18
Robert L. Bocchino Jr <bocchino@jpl.nasa.gov>
2019-10-08 15:07:30 -0700

    Add definitions

--- Definitions/defs.sh
+++ Definitions/defs.sh
@@ -8,9 +8,10 @@ redo-ifchange defs.sh
 
 export FILES="
 Introduction.adoc
-Syntax-Notation.adoc
-Lexical-Elements.adoc
-Types.adoc
-Expressions.adoc
-Definitions/Definitions.adoc
+Array-Definitions.adoc
+Constant-Definitions.adoc
+Enum-Definitions.adoc
+Enumerated-Constant-Definitions.adoc
+Struct-Definitions.adoc
+Namespace-Definitions.adoc
 "
