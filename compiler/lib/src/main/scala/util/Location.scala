cbf6d7812b51382b1c287915efd9ef8cab1db1c5
bocchino <bocchino@jpl.nasa.gov>
2020-03-09 09:14:51 -0700

    Revise Location

--- compiler/lib/src/main/scala/util/Location.scala
+++ compiler/lib/src/main/scala/util/Location.scala
@@ -6,23 +6,31 @@ import util.parsing.input.Position
 final case class Location(
   file: File, /* The file */
   pos: Position, /* The position */
-  includes: List[(File, Position)] = Nil /* The included files and positions, starting with a where A includes B */
+  includes: List[(File, Position)] = Nil /* The included locations: A :: B means A includes B */
 ) {
-    override def toString = {
-      def showIncludes(fpl: List[(File, Position)], s: String): String = { 
-        fpl match {
-          case Nil => s
-          case (file, pos) :: fpl1 => showIncludes(
-            fpl1, 
-            s"\n  included at ${file}: ${pos}"
-          )
-        }
+
+  override def toString = {
+    def showIncludes(fpl: List[(File, Position)], s: String): String = { 
+      fpl match {
+        case Nil => s
+        case (file, pos) :: fpl1 => showIncludes(
+          fpl1, 
+          s"\n  included at ${file}: ${pos}"
+        )
       }
-      val s1 = pos match {
-        case scala.util.parsing.input.NoPosition => s"${file}: end of input"
-        case _ => s"${file}: ${pos.toString}\n${pos.longString}"
-      }
-      val s2 = showIncludes(includes, "")
-      s1 ++ s2
     }
+    val s1 = pos match {
+      case scala.util.parsing.input.NoPosition => s"${file}: end of input"
+      case _ => s"${file}: ${pos.toString}\n${pos.longString}"
+    }
+    val s2 = showIncludes(includes, "")
+    s1 ++ s2
+  }
+
+  /** Get the location of the associated translation unit */
+  def getTULocation: (File, Position) = this.includes match {
+    case Nil => (file, pos)
+    case fp :: _ => fp
+  }
+
 }
