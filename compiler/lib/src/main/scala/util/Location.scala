65278556728b5eab0637db0663dc902e7b38cefb
bocchino <bocchino@jpl.nasa.gov>
2020-03-09 08:54:43 -0700

    Revise Location
    Add included files and positions

--- compiler/lib/src/main/scala/util/Location.scala
+++ compiler/lib/src/main/scala/util/Location.scala
@@ -3,9 +3,26 @@ package fpp.compiler.util
 import util.parsing.input.Position
 
 /** A location used in compilation */
-final case class Location(file: File, pos: Position) {
-    override def toString = pos match {
-      case scala.util.parsing.input.NoPosition => s"${file}: end of input"
-      case _ => s"${file}: ${pos.toString}\n${pos.longString}"
+final case class Location(
+  file: File, /* The file */
+  pos: Position, /* The position */
+  includes: List[(File, Position)] = Nil /* The included files and positions, starting with a where A includes B */
+) {
+    override def toString = {
+      def showIncludes(fpl: List[(File, Position)], s: String): String = { 
+        fpl match {
+          case Nil => s
+          case (file, pos) :: fpl1 => showIncludes(
+            fpl1, 
+            s"  included at ${file}: ${pos}\n"
+          )
+        }
+      }
+      val s1 = pos match {
+        case scala.util.parsing.input.NoPosition => s"${file}: end of input"
+        case _ => s"${file}: ${pos.toString}\n${pos.longString}"
+      }
+      val s2 = showIncludes(includes, "")
+      s1 ++ s2
     }
 }
