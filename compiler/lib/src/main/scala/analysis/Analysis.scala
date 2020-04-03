40db4d600e5c1fee786dda1e9f06df5e2ec7a5d3
bocchino <bocchino@jpl.nasa.gov>
2020-04-03 09:42:04 -0700

    Add Analysis data structure

---
+++ compiler/lib/src/main/scala/analysis/Analysis.scala
@@ -0,0 +1,30 @@
+package fpp.compiler.analysis
+
+import fpp.compiler.util._
+
+/** The analysis data structure */
+case class Analysis(
+  /** The set of files presented to the analyzer */
+  inputFileSet: Set[File] = Set(),
+  /** The set of files on which the analysis depends */
+  dependencyFileSet: Set[File] = Set(),
+  /** The set of files included when parsing input */
+  includedFileSet: Set[File] = Set(),
+  /** A map from qualified names to files.
+   *  Each entry in the map represents the location of a symbol */
+  locationSpecifierMap: Map[Name.Qualified, File] = Map(),
+  /** A list of unqualified names representing the enclosing module
+   *  scopes. For exapmle, inside module B where B is inside A and
+   *  A is at the top level, the module name list is [ A, B ]. */
+  moduleNameList: List[Name.Unqualified] = List(),
+  // TODO
+) {
+
+
+}
+
+object Analysis {
+
+
+
+}
---
+++ compiler/lib/src/main/scala/analysis/Name.scala
@@ -0,0 +1,49 @@
+package fpp.compiler.analysis
+
+import fpp.compiler.ast._
+import fpp.compiler.util._
+
+/** A qualified or unqualified name */
+object Name {
+
+  type Unqualified = String
+
+  case class Qualified(
+    qualifier: List[Unqualified],
+    base: Unqualified
+  ) {
+
+    /** Convert a qualified name to a string */
+    override def toString = {
+      def f(s1: String, s2: String) = s1 ++ "." ++ s2
+      def convertQualifier = qualifier match {
+        case Nil => ""
+        case head :: tail => (tail.fold (head) (f)) ++ "."
+      }
+      convertQualifier ++ base
+    }
+
+    /** Convert a qualified name to an identifier list */
+    def toIdentList = (base :: qualifier).reverse
+
+  }
+
+  object Qualified {
+
+    /** Create a qualified name from a string */
+    def fromString(s: String) = fromIdentList(s.split(".").toList)
+
+    /** Create a qualified name from an identifer list */
+    def fromIdentList(il: List[Ast.Ident]) = {
+      il.reverse match {
+        case head :: tail => Qualified(tail, head)
+        case _ => throw new InternalError("empty identifier list")
+      }
+    }
+
+    /** Create a qualified name from an identifier */
+    def fromIdent(id: Ast.Ident) = Qualified(Nil, id)
+
+  }
+
+}
