13f3dbeef60cbfd84f3ff952c85bfac4249aaf96
bocchino <bocchino@jpl.nasa.gov>
2020-04-25 14:30:07 -0700

    Refactor module analysis
    Move scope management from ModuleAnalyzer into EnterSymbols
    * EnterSymbols is a special case that needs to create scopes
    * We'll create a separate ScopedModuleAnalyzer for managing
      already existing scopes

--- compiler/lib/src/main/scala/analysis/Analyzers/ModuleAnalyzer.scala
+++ compiler/lib/src/main/scala/analysis/Analyzers/ModuleAnalyzer.scala
@@ -12,30 +12,9 @@ trait ModuleAnalyzer extends Analyzer {
   ) = {
     val (_, node1, _) = node
     val Ast.DefModule(name, members) = node1.getData
-    val oldModuleNameList = a.moduleNameList
-    val newModuleNameList = name :: oldModuleNameList
-    val a1 = a.copy(moduleNameList = newModuleNameList)
-    val qualifiedName = Name.Qualified.fromIdentList(newModuleNameList.reverse)
-    val symbol = Symbol.Module(qualifiedName)
-    val (a2, scope) = a1.symbolScopeMap.get(symbol) match {
-      case Some(scope) => (a1, scope)
-      case None => {
-        val scope = Scope.empty
-        val result = a1.nestedScope.put(NameGroup.Value)(name, symbol)
-        val nestedScope = Result.expectRight(result)
-        val a = a1.copy(
-          nestedScope = nestedScope,
-          symbolScopeMap = a1.symbolScopeMap + (symbol -> scope)
-        )
-        (a, scope)
-      }
-    }
-    val a3 = a2.copy(nestedScope = a2.nestedScope.push(scope))
-    for (a <- visitList(a3, members, matchModuleMember))
-    yield a.copy(
-      moduleNameList = oldModuleNameList,
-      nestedScope = a.nestedScope.pop
-    )
+    val a1 = a.copy(moduleNameList = name :: a.moduleNameList)
+    for { a2 <- visitList(a1, members, matchModuleMember) }
+    yield a2.copy(moduleNameList = a.moduleNameList)
   }
 
   override def transUnit(a: Analysis, tu: Ast.TransUnit) =
--- compiler/lib/src/main/scala/analysis/CheckSemantics/EnterSymbols.scala
+++ compiler/lib/src/main/scala/analysis/CheckSemantics/EnterSymbols.scala
@@ -55,6 +55,38 @@ object EnterSymbols extends ModuleAnalyzer {
     yield a.copy(nestedScope = a.nestedScope.pop)
   }
 
+  override def defModuleAnnotatedNode(
+    a: Analysis,
+    node: Ast.Annotated[AstNode[Ast.DefModule]]
+  ) = {
+    val (_, node1, _) = node
+    val Ast.DefModule(name, members) = node1.getData
+    val oldModuleNameList = a.moduleNameList
+    val newModuleNameList = name :: oldModuleNameList
+    val a1 = a.copy(moduleNameList = newModuleNameList)
+    val qualifiedName = Name.Qualified.fromIdentList(newModuleNameList.reverse)
+    val symbol = Symbol.Module(qualifiedName)
+    val (a2, scope) = a1.symbolScopeMap.get(symbol) match {
+      case Some(scope) => (a1, scope)
+      case None => {
+        val scope = Scope.empty
+        val result = a1.nestedScope.put(NameGroup.Value)(name, symbol)
+        val nestedScope = Result.expectRight(result)
+        val a = a1.copy(
+          nestedScope = nestedScope,
+          symbolScopeMap = a1.symbolScopeMap + (symbol -> scope)
+        )
+        (a, scope)
+      }
+    }
+    val a3 = a2.copy(nestedScope = a2.nestedScope.push(scope))
+    for (a <- visitList(a3, members, matchModuleMember))
+    yield a.copy(
+      moduleNameList = oldModuleNameList,
+      nestedScope = a.nestedScope.pop
+    )
+  }
+
   override def defStructAnnotatedNode(a: Analysis, node: Ast.Annotated[AstNode[Ast.DefStruct]]) = {
     val (_, node1, _) = node
     val data = node1.getData
