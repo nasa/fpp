00b4e4addfce9876c5edb1710bc26e0d2878983e
bocchino <bocchino@jpl.nasa.gov>
2020-04-28 09:21:48 -0700

    Refactor CheckDefUseCycles

--- compiler/lib/src/main/scala/analysis/CheckSemantics/CheckUseDefCycles.scala
+++ compiler/lib/src/main/scala/analysis/CheckSemantics/CheckUseDefCycles.scala
@@ -6,48 +6,62 @@ import fpp.compiler.util._
 /** Check for use-def cycles */
 object CheckUseDefCycles extends UseAnalyzer {
 
-  private def visitDefinition(a: Analysis, symbol: Symbol): Result = {
-    symbol match {
-      case Symbol.Constant(node) => defConstantAnnotatedNode(a, node)
-      case _ => Right(a)
-    }
-  }
+  override def constantUse(a: Analysis, node: AstNode[Ast.Expr], use: Name.Qualified): Result =
+    visitUse(a, node, use)
 
-  override def constantUse(a: Analysis, node: AstNode[Ast.Expr], use: Name.Qualified) = {
-    val symbol = a.useDefMap(node.getId)
-    val m = UseDefMatching(node.getId, use, symbol)
-    val a1 = a.copy(useDefMatchingList = m :: a.useDefMatchingList)
-    visitDefinition(a1, symbol)
+  override def defArrayAnnotatedNode(a: Analysis, node: Ast.Annotated[AstNode[Ast.DefArray]]) = {
+    val symbol = Symbol.Array(node)
+    visitDefPost(a, symbol, node, super.defArrayAnnotatedNode)
   }
 
   override def defConstantAnnotatedNode(a: Analysis, node: Ast.Annotated[AstNode[Ast.DefConstant]]) = {
     val symbol = Symbol.Constant(node)
-    val (_, node1, _) = node
-    val data = node1.getData
-    if (a.useDefSymbolSet.contains(symbol)) {
-      val loc = Locations.get(node1.getId)
-      val msg = errorMessage(a)
-      Left(SemanticError.UseDefCycle(loc, msg))
-    }
-    else if (!a.visitedSymbolSet.contains(symbol)) {
-      val a1 = a.copy(useDefSymbolSet = a.useDefSymbolSet + symbol)
-      for (_ <- exprNode(a1, data.value))
-        yield a.copy(visitedSymbolSet = a.visitedSymbolSet + symbol)
-    }
-    else Right(a)
+    visitDefPost(a, symbol, node, super.defConstantAnnotatedNode)
   }
 
   override def transUnit(a: Analysis, tu: Ast.TransUnit) =
     visitList(a, tu.members, matchTuMember)
 
-  override def typeUse(a: Analysis, node: AstNode[Ast.TypeName], use: Name.Qualified) = {
-    // TODO
-    default(a)
+  override def typeUse(a: Analysis, node: AstNode[Ast.TypeName], use: Name.Qualified) =
+    visitUse(a, node, use)
+
+  private def error(a: Analysis, symbol: Symbol): Result = {
+    def msg: String = {
+      def printMatching(m: UseDefMatching) = "\n  " ++ m.toString
+      "encountered a use-def cycle:" ++ a.useDefMatchingList.reverse.map(printMatching).flatten
+    }
+    val loc = symbol.getLoc
+    Left(SemanticError.UseDefCycle(loc, msg))
   }
 
-  private def errorMessage(a: Analysis): String = {
-    def printMatching(m: UseDefMatching) = "\n  " ++ m.toString
-    "encountered a use-def cycle:" ++ a.useDefMatchingList.reverse.map(printMatching).flatten
+  private def visitDefPre(a: Analysis, symbol: Symbol): Result = {
+    symbol match {
+      case Symbol.Array(node) => defArrayAnnotatedNode(a, node)
+      case Symbol.Constant(node) => defConstantAnnotatedNode(a, node)
+      case _ => Right(a)
+    }
+  }
+
+  private def visitDefPost[T](
+    a: Analysis,
+    symbol: Symbol,
+    node: T,
+    visitor: (Analysis, T) => Result
+  ): Result = {
+    if (a.useDefSymbolSet.contains(symbol)) error(a, symbol)
+    else if (!a.visitedSymbolSet.contains(symbol)) {
+      val a1 = a.copy(useDefSymbolSet = a.useDefSymbolSet + symbol)
+      for (_ <- visitor(a1, node))
+      yield a.copy(visitedSymbolSet = a.visitedSymbolSet + symbol)
+    }
+    else Right(a)
+  }
+
+  private def visitUse[T](a: Analysis, node: AstNode[T], use: Name.Qualified): Result = {
+    val symbol = a.useDefMap(node.getId)
+    val m = UseDefMatching(node.getId, use, symbol)
+    val a1 = a.copy(useDefMatchingList = m :: a.useDefMatchingList)
+    visitDefPre(a1, symbol)
   }
 
 }
