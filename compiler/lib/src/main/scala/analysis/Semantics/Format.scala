62cfa60868df31e0e69159052a7d072e3f927ca0
bocchino <bocchino@jpl.nasa.gov>
2020-05-14 18:23:18 -0700

    Revise Format

--- compiler/lib/src/main/scala/analysis/Semantics/Format.scala
+++ compiler/lib/src/main/scala/analysis/Semantics/Format.scala
@@ -2,13 +2,14 @@ package fpp.compiler.analysis
 
 import fpp.compiler.ast._
 import fpp.compiler.util._
+import scala.util.parsing.combinator.RegexParsers
 
 /** An FPP presentation format */
 case class Format(
   /** The first part of the format, before any fields */
   prefix: String,
   /** The list of pairs of fields followed by suffix strings */
-  pairs: List[(Format.Field,String)]
+  fields: List[(Format.Field,String)]
 )
 
 object Format {
@@ -19,9 +20,9 @@ object Format {
 
     case object Default extends Field
 
-    case class Int(t: Int.Type) extends Field
+    case class Integer(t: Integer.Type) extends Field
 
-    object Int {
+    object Integer {
       sealed trait Type
       case object Binary extends Type
       case object Character extends Type
@@ -30,15 +31,47 @@ object Format {
       case object Octal extends Type
     }
 
-    case class Float(precision: Int, t: Float.Type) extends Field
+    case class Floating(precision: Option[Int], t: Floating.Type) extends Field
 
-    object Float {
+    object Floating {
       sealed trait Type
       case object Exponent extends Type
       case object Fixed extends Type
       case object General extends Type
       case object Percent extends Type
     }
+  }
+
+  /** Parse a format string into a format */
+  object Parser extends RegexParsers {
+
+    override def skipWhitespace = false
+
+    def string: Parser[String] = "([^{}]|{{|}})*".r
+
+    def precision: Parser[Int] = "." ~>! "[0-9]+".r  ^^ { _.toInt }
+
+    def field: Parser[Field] = {
+      def integer = {
+        def binary = "b" ^^ { case _ => Field.Integer(Field.Integer.Binary) }
+        def character = "c" ^^ { case _ => Field.Integer(Field.Integer.Character) }
+        def decimal = "d" ^^ { case _ => Field.Integer(Field.Integer.Decimal) }
+        def hexadecimal = "x" ^^ { case _ => Field.Integer(Field.Integer.Hexadecimal) }
+        binary | character | decimal | hexadecimal
+      }
+      def floating = {
+        def ty = {
+          def exponent = "E|e".r ^^ { case _ => Field.Floating.Exponent }
+          def fixed = "F|f".r ^^ { case _ => Field.Floating.Fixed }
+          def general = "G|g".r ^^ { case _ => Field.Floating.General }
+          def percent = "%" ^^ { case _ => Field.Floating.Percent }
+          exponent | fixed | general | percent
+        }
+        opt(precision) ~ ty ^^ { case p ~ t => Field.Floating(p, t) }
+      }
+      "{" ~>! (integer | floating) <~! "}"
+    }
+
   }
 
 }
