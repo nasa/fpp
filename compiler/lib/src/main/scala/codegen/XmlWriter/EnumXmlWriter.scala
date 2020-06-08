678b01fabc4005806fddf92b11238054fb3a21a4
bocchino <bocchino@jpl.nasa.gov>
2020-06-08 15:13:49 -0700

    Revise EnumXmlWriter

--- compiler/lib/src/main/scala/codegen/XmlWriter/EnumXmlWriter.scala
+++ compiler/lib/src/main/scala/codegen/XmlWriter/EnumXmlWriter.scala
@@ -17,8 +17,31 @@ object EnumXmlWriter extends AstVisitor with LineUtils {
       val pairs = s.getNamespaceAndName(data.name)
       XmlTags.tags("enum", pairs)
     }
-    val body = Nil
+    val body = {
+      // The Enum XML schema currently does not allow a top-level comment
+      val comment = Nil
+      val members = data.constants.map(defEnumConstantAnnotatedNode(s, _))
+      comment ++ members
+    }
     XmlTags.taggedLines(tags)(body.map(indentIn))
+  }
+
+  def defEnumConstantAnnotatedNode(
+    s: XmlWriterState,
+    aNode: Ast.Annotated[AstNode[Ast.DefEnumConstant]]
+  ): Line = {
+    val node = aNode._2
+    val data = node.getData
+    val namePair = ("name", data.name)
+    val valuePair = {
+      val Value.EnumConstant(value, _) = s.a.valueMap(node.getId)
+      ("value", value._2.toString)
+    }
+    val pairs = AnnotationXmlWriter.singleLineComment(aNode) match {
+      case Some(commentPair) => List(namePair, valuePair, commentPair)
+      case None => List(namePair, valuePair)
+    }
+    line(XmlTags.openCloseTag("item", pairs))
   }
 
   type In = XmlWriterState
