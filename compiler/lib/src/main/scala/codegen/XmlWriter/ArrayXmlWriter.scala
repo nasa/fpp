1bdbb3dfe75b51bfe009b8b7ee3fed7f138d74aa
jishii <jordan.ishii@github.jpl.nasa.gov>
2020-06-24 10:37:48 -0700

    ArrayXmlWriter

--- compiler/lib/src/main/scala/codegen/XmlWriter/ArrayXmlWriter.scala
+++ compiler/lib/src/main/scala/codegen/XmlWriter/ArrayXmlWriter.scala
@@ -1,1 +1,20 @@
-// START
+package fpp.compiler.codegen
+
+import fpp.compiler.analysis._
+import fpp.compiler.ast._
+import fpp.compiler.util._
+
+/** Write out F Prime XML for array definitions */
+object ArrayXmlWriter extends AstVisitor with LineUtils {
+
+  override def default(s: XmlWriterState) = Nil
+
+  override def defArrayAnnotatedNode(s: XmlWriterState, aNode: Ast.Annotated[AstNode[Ast.DefArray]]) = {
+      Nil
+  }
+
+  type In = XmlWriterState
+
+  type Out = List[Line]
+
+}
