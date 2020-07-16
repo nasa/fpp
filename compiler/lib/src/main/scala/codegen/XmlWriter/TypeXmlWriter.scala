222537225207bfb88b5f14b5e1f3fce142bcbe2c
bocchino <bocchino@jpl.nasa.gov>
2020-07-15 19:13:21 -0700

    Rename files and modules

--- compiler/lib/src/main/scala/codegen/XmlWriter/TypeXmlWriter.scala
+++ compiler/lib/src/main/scala/codegen/XmlWriter/TypeXmlWriter.scala
@@ -1,64 +1,61 @@
 package fpp.compiler.codegen
 
-import fpp.compiler.ast._
+import fpp.compiler.analysis._
 import fpp.compiler.util._
 
 /** Write an FPP type as XML */
 object TypeXmlWriter {
 
-  private object Visitor extends AstVisitor {
+  private object NameVisitor extends TypeVisitor {
 
-    override def default(u: Unit) = throw new InternalError("visitor not defined")
+    override def absType(s: XmlWriterState, t: Type.AbsType) =
+      symbol(s, Symbol.AbsType(t.node))
 
-    override def typeNameBoolNode(u: Unit, node: AstNode[Ast.TypeName]) = "bool"
+    override def array(s: XmlWriterState, t: Type.Array) =
+      symbol(s, Symbol.Array(t.node))
 
-    override def typeNameFloatNode(u: Unit, node: AstNode[Ast.TypeName], tn: Ast.TypeNameFloat) =
-      tn.name match {
-        case Ast.F32() => "F32"
-        case Ast.F64() => "F64"
-      }
+    override def boolean(s: XmlWriterState) = "bool"
 
-    override def typeNameIntNode(u: Unit, node: AstNode[Ast.TypeName], tn: Ast.TypeNameInt) =
-      tn.name match {
-        case Ast.I8() => "I8"
-        case Ast.I16() => "I16"
-        case Ast.I32() => "I32"
-        case Ast.I64() => "I64"
-        case Ast.U8() => "U8"
-        case Ast.U16() => "U16"
-        case Ast.U32() => "U32"
-        case Ast.U64() => "U64"
-      }
+    override def default(s: XmlWriterState, t: Type) = throw new InternalError("visitor not defined")
 
-    override def typeNameQualIdentNode(u: Unit, node: AstNode[Ast.TypeName], tn: Ast.TypeNameQualIdent) = {
-      def qualIdent(qi: Ast.QualIdent): String = qi match {
-        case Ast.QualIdent.Unqualified(name) => name
-        case Ast.QualIdent.Qualified(qualifier, name) => s"${qualIdent(qualifier.getData)}::${name.getData}"
-      }
-      qualIdent(tn.name.getData)
-    }
+    override def enum(s: XmlWriterState, t: Type.Enum) =
+      symbol(s, Symbol.Enum(t.node))
 
-    override def typeNameStringNode(u: Unit, node: AstNode[Ast.TypeName], tn: Ast.TypeNameString) = "string"
+    override def float(s: XmlWriterState, t: Type.Float) = t.toString
 
-    type In = Unit
+    override def primitiveInt(s: XmlWriterState, t: Type.PrimitiveInt) = t.toString
+
+    override def string(s: XmlWriterState, t: Type.String) = "string"
+
+    override def struct(s: XmlWriterState, t: Type.Struct) =
+      symbol(s, Symbol.Struct(t.node))
+
+    type In = XmlWriterState
 
     type Out = String
+
+    private def symbol(s: XmlWriterState, sym: Symbol): String = {
+      val qualifiedName = s.a.qualifiedNameMap(sym)
+      val shortName = s.a.shortName(qualifiedName)
+      shortName.toString.replaceAll(".", "::")
+    }
+
   }
 
   /** Get the name of a type */
-  def getName(s: XmlWriterState, tn: AstNode[Ast.TypeName]): String = Visitor.matchTypeNameNode((), tn)
+  def getName(s: XmlWriterState, t: Type): String = NameVisitor.ty(s, t)
 
   /** Get the size of a type */
-  def getSize(s: XmlWriterState, tn: AstNode[Ast.TypeName]): Option[String] = tn.getData match {
-    case Ast.TypeNameString(Some(node)) => Some(s.a.valueMap(node.getId).toString)
-    case Ast.TypeNameString(_) => Some(s.defaultStringSize.toString)
+  def getSize(s: XmlWriterState, t: Type): Option[String] = t match {
+    case Type.String(Some(node)) => Some(s.a.valueMap(node.getId).toString)
+    case Type.String(None) => Some(s.defaultStringSize.toString)
     case _ => None
   }
 
   /** Get the key-value pairs for a type */
-  def getPairs(s: XmlWriterState, tn: AstNode[Ast.TypeName]): List[(String,String)] = {
-    val name = ("type", getName(s, tn))
-    getSize(s, tn) match {
+  def getPairs(s: XmlWriterState, t: Type): List[(String,String)] = {
+    val name = ("type", getName(s, t))
+    getSize(s, t) match {
       case Some(size) => List(name, ("size", size))
       case None => List(name)
     }
