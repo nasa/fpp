2122fe8e42c6c9f0067f3a73f47ba04739004dd9
bocchino <bocchino@jpl.nasa.gov>
2020-08-03 13:52:06 -0700

    Revise User's Guide
    Add outline of sections

--- docs/users-guide/defs.sh
+++ docs/users-guide/defs.sh
@@ -12,4 +12,8 @@ Installing-FPP.adoc
 Defining-Constants.adoc
 Defining-Types.adoc
 Defining-Enums.adoc
+Specifying-Models.adoc
+Analyzing-Models.adoc
+Translating-Models.adoc
+F-Prime-XML-Format.adoc
 "
