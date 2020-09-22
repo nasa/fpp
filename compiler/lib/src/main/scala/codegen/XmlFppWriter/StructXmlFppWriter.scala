ec57f050b070055c1443931426e5574628699d61
bocchino <bocchino@jpl.nasa.gov>
2020-09-22 13:40:16 -0700

    Revise fpp-from-xml
    Fix format translation for inline arrays

--- compiler/lib/src/main/scala/codegen/XmlFppWriter/StructXmlFppWriter.scala
+++ compiler/lib/src/main/scala/codegen/XmlFppWriter/StructXmlFppWriter.scala
@@ -24,13 +24,13 @@ object StructXmlFppWriter extends LineUtils {
 
     /** Extracts a struct type member */
     def structTypeMemberAnnotatedNode(file: XmlFppWriter.File, node: scala.xml.Node): 
-      Result.Result[Ast.Annotated[AstNode[Ast.StructTypeMember]]] =
+      Result.Result[Ast.Annotated[AstNode[Ast.StructTypeMember]]] = {
+      val sizeOpt = XmlFppWriter.getAttributeOpt(node, "size")
       for {
         structName <- file.getAttribute(file.elem, "name")
         memberName <- file.getAttribute(node, "name")
         xmlType <- file.getAttribute(node, "type")
         memberType <- {
-          val sizeOpt = XmlFppWriter.getAttributeOpt(node, "size")
           (xmlType, sizeOpt) match {
             case ("string", _) => translateType(file)(node)
             case (_, Some(size)) => {
@@ -43,7 +43,10 @@ object StructXmlFppWriter extends LineUtils {
         }
       }
       yield {
-        val xmlFormatOpt = XmlFppWriter.getAttributeOpt(node, "format")
+        val xmlFormatOpt = sizeOpt match {
+          case Some(_) => None
+          case None => XmlFppWriter.getAttributeOpt(node, "format")
+        }
         val (fppFormatOpt, pre) = XmlFppWriter.FppBuilder.translateFormatOpt(xmlFormatOpt)
         val data = Ast.StructTypeMember(
           memberName,
@@ -54,6 +57,7 @@ object StructXmlFppWriter extends LineUtils {
         val post = XmlFppWriter.getAttributeComment(node)
         (pre, astNode, post)
       }
+    }
 
     /** Extracts an array definition from a struct member if needed */
     def defArrayAnnotatedOpt(file: XmlFppWriter.File)(structName:String)(node: scala.xml.Node):
@@ -63,21 +67,22 @@ object StructXmlFppWriter extends LineUtils {
         xmlType <- file.getAttribute(node, "type")
         result <- {
           val sizeOpt = XmlFppWriter.getAttributeOpt(node, "size")
+          val xmlFormatOpt = XmlFppWriter.getAttributeOpt(node, "format")
+          val (fppFormatOpt, pre) = XmlFppWriter.FppBuilder.translateFormatOpt(xmlFormatOpt)
           (xmlType, sizeOpt) match {
             case ("string", _) => Right(None)
             case (_, None) => Right(None)
-            case (_, Some(size)) => 
-              for (memberType <- translateType(file)(node))
-                yield {
-                  val array = Ast.DefArray(
-                    getArrayName(structName, memberName),
-                    AstNode.create(Ast.ExprLiteralInt(size)),
-                    AstNode.create(memberType),
-                    None,
-                    None
-                  )
-                  Some((Nil, array, Nil))
-                }
+            case (_, Some(size)) => for (memberType <- translateType(file)(node))
+              yield {
+                val array = Ast.DefArray(
+                  getArrayName(structName, memberName),
+                  AstNode.create(Ast.ExprLiteralInt(size)),
+                  AstNode.create(memberType),
+                  None,
+                  fppFormatOpt.map(AstNode.create(_))
+                )
+                Some((pre, array, Nil))
+              }
           }
         }
       }
