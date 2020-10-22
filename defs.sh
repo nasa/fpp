a393fde35be9d7a07578942da5c7c60b044ff10c
bocchino <bocchino@jpl.nasa.gov>
2020-10-22 15:38:50 -0700

    Update version number to v0.2.1

--- defs.sh
+++ defs.sh
@@ -2,7 +2,7 @@
 # defs.sh: definitions common to all subdirectories
 # ----------------------------------------------------------------------
 
-export VERSION=v0.2.0
+export VERSION=v0.2.1
 
 # Users of this file should set LEVEL first
 if test -z "$LEVEL"; then
