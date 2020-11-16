a41d2a15a51cd7444764fc194c9814004cc47c4d
bocchino <bocchino@jpl.nasa.gov>
2020-11-16 10:57:44 -0800

    Add Event

---
+++ compiler/lib/src/main/scala/analysis/Semantics/Event.scala
@@ -0,0 +1,21 @@
+package fpp.compiler.analysis
+
+import fpp.compiler.ast._
+
+/** An FPP event */
+final case class Event(
+  aNode: Ast.Annotated[AstNode[Ast.SpecEvent]],
+  format: Option[Format],
+  throttle: Option[Value]
+) {
+
+  /** Gets the location of the event */
+  def getLoc = Locations.get(aNode._2.id)
+
+}
+
+final object Event {
+
+  type Id = Int
+
+}
