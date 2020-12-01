c1beedd02350f4fdeeac6cb97347a408a6265d5d
bocchino <bocchino@jpl.nasa.gov>
2020-12-01 14:23:42 -0800

    Reorganize fpp-to-xml tests

---
+++ compiler/tools/fpp-to-xml/test/struct/enum_explicit.fpp
@@ -0,0 +1,9 @@
+module M {
+
+  @ An enum with explicit constant values
+  enum Explicit {
+    X = 1 @< Member X
+    Y = 2
+  }
+
+}
---
+++ compiler/tools/fpp-to-xml/test/struct/tests.sh
@@ -0,0 +1,11 @@
+tests="
+enum_explicit
+struct_abs_type
+struct_enum_member
+struct_format
+struct_modules
+struct_ok
+struct_output_dir
+struct_string
+"
+
