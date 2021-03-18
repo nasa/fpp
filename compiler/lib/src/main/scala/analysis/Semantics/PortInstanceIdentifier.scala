d7e3e30b9ce5337c6f29b92e73a2409ccaed4a6e
bocchino <bocchino@jpl.nasa.gov>
2021-03-18 14:30:16 -0700

    Add Port Instance Identifier

---
+++ compiler/lib/src/main/scala/analysis/Semantics/PortInstanceIdentifier.scala
@@ -0,0 +1,14 @@
+package fpp.compiler.analysis
+
+import fpp.compiler.ast._
+import fpp.compiler.util._
+
+/** An FPP port instance identifier */
+case class PortInstanceIdentifier(
+  /** The AST node defining the identifier */
+  node: AstNode[Ast.PortInstanceIdentifier],
+  /** The component instance */
+  componentInstance: ComponentInstance,
+  /** The port instance name */
+  portName: String
+)
