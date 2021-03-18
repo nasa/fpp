980189b8758e87a41b8b3339a32517b6c948174b
bocchino <bocchino@jpl.nasa.gov>
2021-03-17 21:54:56 -0700

    Revise Check Topology Defs

--- compiler/lib/src/main/scala/analysis/CheckSemantics/CheckTopologyDefs.scala
+++ compiler/lib/src/main/scala/analysis/CheckSemantics/CheckTopologyDefs.scala
@@ -14,14 +14,30 @@ object CheckTopologyDefs
     a: Analysis,
     aNode: Ast.Annotated[AstNode[Ast.DefTopology]]
   ) = {
-    val a1 = a.copy(topology = Some(Topology(aNode)))
-    for {
-      a <- super.defTopologyAnnotatedNode(a1, aNode)
-      t <- a.topology.get.complete
-    }
-    yield {
-      val symbol = Symbol.Topology(aNode)
-      a.copy(topologyMap = a.topologyMap + (symbol -> t))
+    val symbol = Symbol.Topology(aNode)
+    a.topologyMap.get(symbol) match {
+      case None =>
+        // Topology is not in the map: visit it
+        val a1 = a.copy(topology = Some(Topology(aNode)))
+        for {
+          // Visit topology members and compute top
+          a <- super.defTopologyAnnotatedNode(a1, aNode)
+          top <- Right(a.topology.get)
+          a <- {
+            // Visit topologies imported by top
+            val tops = top.importedTopologyMap.toList
+            Result.foldLeft (tops) (a) ((a, tl) => {
+              defTopologyAnnotatedNode(a, tl._1.aNode)
+            })
+          }
+          // Complete top
+          top <- top.complete
+        }
+        yield a.copy(topologyMap = a.topologyMap + (symbol -> top))
+      case _ => {
+        // Topology is already in the map: nothing to do
+        Right(a)
+      }
     }
   }
 
