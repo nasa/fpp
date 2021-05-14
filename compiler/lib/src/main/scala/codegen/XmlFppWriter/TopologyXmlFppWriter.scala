fefccd4d1a4ed2aa24b2208058af3b2e460a266b
bocchino <bocchino@jpl.nasa.gov>
2021-05-14 16:34:09 -0700

    Revise topology XML FPP writer
    Add FPP generation for component instance defs

--- compiler/lib/src/main/scala/codegen/XmlFppWriter/TopologyXmlFppWriter.scala
+++ compiler/lib/src/main/scala/codegen/XmlFppWriter/TopologyXmlFppWriter.scala
@@ -14,11 +14,42 @@ object TopologyXmlFppWriter extends LineUtils {
   /** Builds FPP for translating topology XML */
   private object FppBuilder {
 
-    /** Translates the component instances */
+    /** Translates a component instance definition */
+    def defComponentInstanceAnnotated(
+      file: XmlFppWriter.File,
+      node: scala.xml.Node
+    ): Result.Result[Ast.Annotated[Ast.DefComponentInstance]] =
+      for {
+        name <- file.getAttribute(node, "name")
+        componentName <- file.getAttribute(node, "type")
+        baseId <- file.getAttribute(node, "base_id")
+      }
+      yield {
+        val componentQid = Ast.QualIdent.fromNodeList(
+          (XmlFppWriter.getAttributeNamespace(node) :+ componentName).
+            map(AstNode.create(_))
+        )
+        (
+          Nil,
+          Ast.DefComponentInstance(
+            name,
+            AstNode.create(componentQid),
+            AstNode.create(Ast.ExprLiteralInt(baseId)),
+            None,
+            None,
+            None,
+            None
+          ),
+          Nil
+        )
+      }
+
+    /** Translates the component instances definitions */
     def defComponentInstanceList(file: XmlFppWriter.File):
-      Result.Result[List[Ast.Annotated[Ast.DefComponentInstance]]] = 
-        // TODO
-        Right(Nil)
+      Result.Result[List[Ast.Annotated[Ast.DefComponentInstance]]] = { 
+        val instances = file.elem \ "instance"
+        Result.map(instances.toList, defComponentInstanceAnnotated(file, _))
+      }
 
     /** Translates the topology */
     def defTopologyAnnotated(file: XmlFppWriter.File):
