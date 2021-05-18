1890f4aa340855ddca957fb47600ae194d1023e2
bocchino <bocchino@jpl.nasa.gov>
2021-05-17 17:33:10 -0700

    Add topology test for fpp-from-xml

--- compiler/scripts/utils.sh
+++ compiler/scripts/utils.sh
@@ -5,7 +5,7 @@ clean()
 {
   echo "cleaning $PWD"
   rm -f num_failed.txt test-output.txt
-  for file in `find . -name '*.out.txt' -or -name '*.diff.txt' -or -name '*~'`
+  for file in `find . -name '*.out.txt' -or -name '*.diff.txt' -or -name '*~' -or -name 'default-*sh'`
   do
     rm $file
   done
---
+++ compiler/tools/fpp-from-xml/test/top/tests.sh
@@ -0,0 +1,3 @@
+tests="
+fprime_ref
+"
