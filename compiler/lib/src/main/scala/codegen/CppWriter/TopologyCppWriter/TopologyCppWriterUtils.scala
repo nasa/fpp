ac64456c470b94a3655364b9b9758485e19ecd2f
bocchino <bocchino@jpl.nasa.gov>
2021-05-28 13:50:34 -0700

    Add missing file

---
+++ compiler/lib/src/main/scala/codegen/CppWriter/TopologyCppWriter/TopologyCppWriterUtils.scala
@@ -0,0 +1,56 @@
+package fpp.compiler.codegen
+
+import fpp.compiler.analysis._
+import fpp.compiler.ast._
+import fpp.compiler.util._
+
+/** Utilities for writing C++ topology definitions */
+abstract class TopologyCppWriterUtils(
+  s: CppWriterState,
+  aNode: Ast.Annotated[AstNode[Ast.DefTopology]]
+) extends LineUtils {
+
+  val symbol = Symbol.Topology(aNode)
+
+  val namespace = s.getNamespace(symbol)
+
+  val name = aNode._2.data.name
+
+  val t = s.a.topologyMap(symbol)
+
+  val instances = t.instanceMap.keys.toList.sorted
+
+  def addBannerComment(comment: String, ll: List[Line]): List[Line] =
+    ll match {
+      case Nil => Nil
+      case _ => CppDocWriter.writeBannerComment(comment) ++ ll
+    }
+
+  def getShortName(name: Name.Qualified) = {
+    val ens = s.a.getEnclosingNames(symbol)
+    name.shortName(ens)
+  }
+
+  def wrapInScope(
+    s1: String,
+    ll: List[Line],
+    s2: String
+  ): List[Line] = ll match {
+    case Nil => Nil
+    case _ => List(lines(s1), ll.map(indentIn), lines(s2)).flatten
+  }
+
+  def wrapInNamespace(namespace: String, ll: List[Line]): List[Line] =
+    wrapInScope(s"namespace $namespace {", ll, "}")
+
+  def wrapInEnum(ll: List[Line]): List[Line] =
+    wrapInScope("enum {", ll, "}")
+
+  def getSpecifierForPhase (phase: Int) (ci: ComponentInstance): 
+    Option[InitSpecifier] = s.a.initSpecifierMap.getOrElse(ci, Map()).
+      get(phase)
+
+  def getCodeForPhase (phase: Int)(ci: ComponentInstance): Option[String] =
+    getSpecifierForPhase(phase)(ci).map(is => is.aNode._2.data.code)
+
+}
