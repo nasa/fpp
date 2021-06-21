e23d5fee7a12269bb4313ad64f31391781c5745a
bocchino <bocchino@jpl.nasa.gov>
2021-06-21 13:49:05 -0700

    Revise TopPrivateFunctions
    Add code skeleton

--- compiler/lib/src/main/scala/codegen/CppWriter/TopologyCppWriter/TopPrivateFunctions.scala
+++ compiler/lib/src/main/scala/codegen/CppWriter/TopologyCppWriter/TopPrivateFunctions.scala
@@ -8,8 +8,71 @@ import fpp.compiler.util._
 case class TopPrivateFunctions(
   s: CppWriterState,
   aNode: Ast.Annotated[AstNode[Ast.DefTopology]]
-) {
+) extends TopologyCppWriterUtils(s, aNode) {
 
-  def getLines: List[Line] = Nil
+  def getLines: List[Line] = {
+    List(
+      addBannerComment("Initialize components", getInitComponentsLines),
+      addBannerComment("Configure components", getConfigComponentsLines),
+      addBannerComment("Set component base IDs", getSetBaseIDsLines),
+      addBannerComment("Connect components", getConnectComponentsLines),
+      addBannerComment("Register commands", getRegisterCommandsLines),
+      addBannerComment("Load parameters", getLoadParametersLines),
+      addBannerComment("Start tasks", getStartTasksLines),
+      addBannerComment("Stop tasks", getStopTasksLines),
+      addBannerComment("Free threads", getFreeThreadsLines),
+      addBannerComment("Tear down components", getTearDownComponentsLines),
+    ).flatten
+  }
+
+  private def getInitComponentsLines: List[Line] = {
+    // TODO
+    Nil
+  }
+
+  private def getConfigComponentsLines: List[Line] = {
+    // TODO
+    Nil
+  }
+
+  private def getRegisterCommandsLines: List[Line] = {
+    // TODO
+    Nil
+  }
+
+  private def getSetBaseIDsLines: List[Line] = {
+    // TODO
+    Nil
+  }
+
+  private def getConnectComponentsLines: List[Line] = {
+    // TODO
+    Nil
+  }
+
+  private def getLoadParametersLines: List[Line] = {
+    // TODO
+    Nil
+  }
+
+  private def getStartTasksLines: List[Line] = {
+    // TODO
+    Nil
+  }
+
+  private def getStopTasksLines: List[Line] = {
+    // TODO
+    Nil
+  }
+
+  private def getFreeThreadsLines: List[Line] = {
+    // TODO
+    Nil
+  }
+
+  private def getTearDownComponentsLines: List[Line] = {
+    // TODO
+    Nil
+  }
 
 }
