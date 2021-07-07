45d3729d8e7b8dd14c3186d9c972ce47a15fe8b1
bocchino <bocchino@jpl.nasa.gov>
2021-07-06 19:23:44 -0700

    Add C++ check for basic topology

---
+++ compiler/tools/fpp-to-cpp/test/top/check-cpp/Basic/Active.hpp
@@ -0,0 +1,58 @@
+#ifndef M_Active_HPP
+#define M_Active_HPP
+
+#include "ActiveComponentAc.hpp"
+
+namespace M {
+
+  class Active :
+    public ActiveComponentBase
+  {
+
+    public:
+
+      Active() {
+
+      }
+
+      Active(const char* name) {
+
+      }
+
+      void init(int a, int b) {
+
+      }
+
+      void initSpecial() {
+
+      }
+
+      void config() {
+
+      }
+
+      void startSpecial() {
+
+      }
+      
+      void p_handler(NATIVE_INT_TYPE portNum) {
+
+      }
+
+      void stopSpecial() {
+
+      }
+
+      void freeSpecial() {
+
+      }
+
+      void tearDown() {
+
+      }
+
+  };
+
+}
+
+#endif
---
+++ compiler/tools/fpp-to-cpp/test/top/check-cpp/Basic/Passive.hpp
@@ -0,0 +1,26 @@
+#ifndef M_Passive_HPP
+#define M_Passive_HPP
+
+#include "PassiveComponentAc.hpp"
+
+namespace M {
+
+  class Passive :
+    public PassiveComponentBase
+  {
+
+    public:
+
+      Passive(const char* name) {
+
+      }
+
+      void init(int a) {
+
+      }
+
+  };
+
+}
+
+#endif
