2bbf471af2b6353cc26487528e30763f4d49ae27
bocchino <bocchino@jpl.nasa.gov>
2022-01-31 18:08:45 -0800

    Revise version stamp (#108)

--- compiler/lib/src/main/scala/util/Version.scala
+++ compiler/lib/src/main/scala/util/Version.scala
@@ -3,8 +3,6 @@ package fpp.compiler.util
 /** The compiler version */
 object Version {
 
-  val tag = "v1.0.0"
-  val commit = "[unknown]"
-  val v = s"$tag commit $commit"
+  val v = "[unknown]"
 
 }
