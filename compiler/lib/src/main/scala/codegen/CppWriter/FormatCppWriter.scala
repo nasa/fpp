78de14d57a65bb66572d5ee72d968e5df1a29d85
tiffany1618 <tiffany9342@gmail.com>
2022-07-07 10:09:51 -0700

    Use PRI macros for C++ format strings

--- compiler/lib/src/main/scala/codegen/CppWriter/FormatCppWriter.scala
+++ compiler/lib/src/main/scala/codegen/CppWriter/FormatCppWriter.scala
@@ -4,41 +4,30 @@ import fpp.compiler.analysis._
 import fpp.compiler.ast._
 import fpp.compiler.util._
 
-/** Write an FPP format as C++ */
+/** Write an FPP format as C++
+ *  Uses PRI macros from cinttypes header for formatting integer types */
 object FormatCppWriter {
+
+  /** Write an FPP format field as a C++ format string */
   def writeField(f: Format.Field, tn: AstNode[Ast.TypeName]): String = {
     import Format.Field._
+    def getDecimalFormat(name: Ast.TypeInt) = (TypeUtils.signedness(name), TypeUtils.width(name)) match {
+      case (TypeUtils.Unsigned, w) => s"%\" PRIu${w.toString} \""
+      case (TypeUtils.Signed, w) => s"%\" PRIi${w.toString} \""
+    }
     def default = tn.data match {
       case Ast.TypeNameFloat(_) => "%f"
-      case Ast.TypeNameInt(name) => name match {
-        case Ast.I8()
-           | Ast.I16()
-           | Ast.I32()
-           | Ast.I64() => "%d"
-        case Ast.U8()
-           | Ast.U16()
-           | Ast.U32()
-           | Ast.U64() => "%u"
-      }
+      case Ast.TypeNameInt(name) => getDecimalFormat(name)
       case Ast.TypeNameQualIdent(_) => "%s"
-      case Ast.TypeNameBool => "%d"
+      case Ast.TypeNameBool => "%d" // C++ boolean is promoted to int in printf
       case Ast.TypeNameString(_) => "%s"
     }
     def integer(t: Integer.Type) = tn.data match {
       case Ast.TypeNameInt(name) => t match {
         case Integer.Character => "%c"
-        case Integer.Decimal => name match {
-          case Ast.I8()
-             | Ast.I16()
-             | Ast.I32()
-             | Ast.I64() => "%d"
-          case Ast.U8()
-             | Ast.U16()
-             | Ast.U32()
-             | Ast.U64() => "%u"
-        }
-        case Integer.Hexadecimal => "%x"
-        case Integer.Octal => "%o"
+        case Integer.Decimal => getDecimalFormat(name)
+        case Integer.Hexadecimal => s"%\" PRIx${TypeUtils.width(name).toString} \""
+        case Integer.Octal => s"%\" PRIo${TypeUtils.width(name).toString} \""
       }
       case _ => default
     }
