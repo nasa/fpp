80cbbac79619c82b7c32e803386583d77d1f6c26
tiffany1618 <tiffany9342@gmail.com>
2022-08-04 17:15:26 -0700

    Fix run scripts for C++ writer unit tests

--- compiler/tools/fpp-to-cpp/test/struct/run.sh
+++ compiler/tools/fpp-to-cpp/test/struct/run.sh
@@ -1,21 +1,21 @@
 primitive()
 {
   run_test "-p $PWD" primitive && \
-    diff_cpp PrimitiveSerializable
+    diff_cpp PrimitiveSerializable && \
     diff_cpp PrimitiveStructSerializable
 }
 
 string()
 {
   run_test "-p $PWD" string && \
-    diff_cpp StringSerializable
+    diff_cpp StringSerializable && \
     diff_cpp StringArraySerializable
 }
 
 enum()
 {
   run_test "-p $PWD" enum && \
-    diff_cpp EEnum
+    diff_cpp EEnum && \
     diff_cpp EnumSerializable 
 }
 
@@ -46,14 +46,14 @@ format()
 modules()
 {
   run_test "-p $PWD" modules && \
-    diff_cpp Modules1Serializable
-    diff_cpp Modules2Serializable
+    diff_cpp Modules1Serializable && \
+    diff_cpp Modules2Serializable && \
     diff_cpp Modules3Serializable
 }
 
 component()
 {
   run_test "-p $PWD" component && \
-    diff_cpp C_SSerializable
+    diff_cpp C_SSerializable && \
     diff_cpp SSerializable
 }
