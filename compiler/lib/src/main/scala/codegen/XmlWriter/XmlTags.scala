eb0369c91efa94dc780960ec2dce4ba30f35ad90
bocchino <bocchino@jpl.nasa.gov>
2022-10-26 09:29:07 -0700

    Revise comment

--- compiler/lib/src/main/scala/codegen/XmlWriter/XmlTags.scala
+++ compiler/lib/src/main/scala/codegen/XmlWriter/XmlTags.scala
@@ -10,14 +10,14 @@ object XmlTags extends LineUtils {
   def openTag(name: String, pairs: List[(String, String)] = Nil): String = openTagPrefix(name, pairs) ++ ">"
 
   def quoted(s: String): String = {
-    // If s represents a C++ literal string value, then remove the leading and
-    // trailing quotation marks from the value now. The F Prime autocoder will
+    // If s represents a C++ literal string value, then remove the enclosing
+    // quotation marks from the value now. The F Prime autocoder will
     // re-insert them during code gen. If s represents a non-string value, then
     // the next line is a no-op.
     val s1 = s.replaceAll("^\"", "").replaceAll("\"$", "")
     // Escape any remaining quotation marks in the XML way
     val s2 = s1.replaceAll("\"", "&quot;")
-    // Add the leading and trailing quotation marks for the XML attribute
+    // Add the enclosing quotation marks for the XML attribute
     "\"" ++ s2 ++ "\""
   }
 
