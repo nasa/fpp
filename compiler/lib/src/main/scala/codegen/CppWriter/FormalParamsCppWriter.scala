2c6c34073d21a87281c1e3350e08468d74fb0e2d
Tiffany Chieu <tiffany9342@gmail.com>
2023-02-22 21:28:12 -0800

    Revise FormalParamsCppWriter

--- compiler/lib/src/main/scala/codegen/CppWriter/FormalParamsCppWriter.scala
+++ compiler/lib/src/main/scala/codegen/CppWriter/FormalParamsCppWriter.scala
@@ -4,21 +4,40 @@ import fpp.compiler.analysis._
 import fpp.compiler.ast._
 
 /** Write formal parameters as C++ */
-case class FormalParamsWriter(
+case class FormalParamsCppWriter(
   s: CppWriterState
 ) {
 
+  /** Writes a list of formal parameters as a list of CppDoc Function Params */
+  def write(
+    params: Ast.FormalParamList,
+    namespaceNames: List[String] = Nil,
+    strName: Option[String] = None,
+    passingConvention: FormalParamsCppWriter.SerializablePassingConvention = FormalParamsCppWriter.ConstRef
+  ): List[CppDoc.Function.Param] =
+    params.map(aNode => {
+      CppDoc.Function.Param(
+        getFormalParamType(
+          aNode._2.data,
+          strName,
+          namespaceNames,
+          passingConvention
+        ),
+        aNode._2.data.name,
+        AnnotationCppWriter.asStringOpt(aNode)
+      )
+    })
+
   /** Writes a formal parameter as a C++ parameter */
-  def writeFormalParam(
+  def getFormalParamType(
     param: Ast.FormalParam,
     strName: Option[String] = None,
     namespaceNames: List[String] = Nil,
     passingConvention: FormalParamsCppWriter.SerializablePassingConvention = FormalParamsCppWriter.ConstRef
-  ): String = {
+  ): CppDoc.Type = {
     val t = s.a.typeMap(param.typeName.id)
     val typeName = TypeCppWriter(s, strName, namespaceNames).write(t)
-
-    param.kind match {
+    val qualifiedTypeName = param.kind match {
       // Reference formal parameters become non-constant C++ reference parameters
       case Ast.FormalParam.Ref => s"$typeName&"
       case Ast.FormalParam.Value => t match {
@@ -33,27 +52,9 @@ case class FormalParamsWriter(
         }
       }
     }
-  }
 
-  /** Writes a list of formal parameters as a list of CppDoc Function Params */
-  def writeFormalParamList(
-    params: Ast.FormalParamList,
-    namespaceNames: List[String] = Nil,
-    strName: Option[String] = None,
-    passingConvention: FormalParamsCppWriter.SerializablePassingConvention = FormalParamsCppWriter.ConstRef
-  ): List[CppDoc.Function.Param] =
-    params.map(aNode => {
-      CppDoc.Function.Param(
-        CppDoc.Type(writeFormalParam(
-          aNode._2.data,
-          strName,
-          namespaceNames,
-          passingConvention
-        )),
-        aNode._2.data.name,
-        AnnotationCppWriter.asStringOpt(aNode)
-      )
-    })
+    CppDoc.Type(qualifiedTypeName)
+  }
 
 }
 
@@ -61,9 +62,7 @@ object FormalParamsCppWriter {
 
   /** The passing convention for a serializable type */
   sealed trait SerializablePassingConvention
-
   case object ConstRef extends SerializablePassingConvention
-
   case object Value extends SerializablePassingConvention
 
 }
