9b2d667340501ba034dc1bd8969aa39c6fd0b118
tiffany1618 <tiffany9342@gmail.com>
2023-03-29 17:11:51 -0700

    Add compilation tests for all combinations of F Prime flags for C++ component writer

---
+++ compiler/tools/fpp-to-cpp/test/component/gen_guards.py
@@ -0,0 +1,36 @@
+import sys
+
+def gen_guards():
+    guards_to_remove = ['-DFW_AMPCS_COMPATIBLE', '-DFW_CMD_CHECK_RESIDUAL', '-DFW_ARRAY_TO_STRING']
+
+    guards = sys.argv[1:]
+    for guard in guards_to_remove:
+        if guard in guards:
+            guards.remove(guard)
+
+    guards_list = []
+    num_guards = len(guards)
+    format_str = f"{num_guards:>03}b"
+    i = 0
+
+    while i < (2 ** num_guards):
+        values = format(i, format_str)
+
+        new_guards = []
+        for j in range(num_guards):
+            new_guards.append(f"{guards[j]}={values[j]}")
+            
+            if guards[j] == '-DFW_SERIALIZABLE_TO_STRING':
+                new_guards.append(f"-DFW_ARRAY_TO_STRING={values[j]}")
+
+        guards_list.append(new_guards)
+        i = i + 1
+
+    for guards in guards_list:
+        guards_str = ','.join(guards)
+        sys.stdout.write(f"{guards_str}\n")
+
+
+if __name__ == '__main__':
+    gen_guards()
+
