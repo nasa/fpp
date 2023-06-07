6a87e1d04c5bfd187f7f3a4f0edcaf0cdf9451f9
bocchino <bocchino@jpl.nasa.gov>
2023-06-07 13:55:29 -0700

    Revise dp unit tests

--- compiler/tools/fpp-to-cpp/test/dp/run.sh
+++ compiler/tools/fpp-to-cpp/test/dp/run.sh
@@ -3,6 +3,6 @@ fprime_dir=`dirname $PWD`/fprime
 dp()
 {
   run_test "-i `cat deps-comma.txt`" "-p $PWD,$fprime_dir dp" dp && \
-  diff_cpp DpTestDpComponent && \
+  diff_cpp DpTestComponent && \
   diff_cpp DpTest_DataSerializable
 }
--- compiler/tools/fpp-to-cpp/test/dp/update-ref.sh
+++ compiler/tools/fpp-to-cpp/test/dp/update-ref.sh
@@ -3,6 +3,6 @@ fprime_dir=`dirname $PWD`/fprime
 dp()
 {
   update "-i `cat deps-comma.txt`" "-p $PWD,$fprime_dir dp" dp
-  move_cpp DpTestDpComponent
+  move_cpp DpTestComponent
   move_cpp DpTest_DataSerializable
 }
