6958a1c425d56195d3b801123650c39c30f9adaf
Elliot Fayman <42043588+elliotfayman@users.noreply.github.com>
2023-06-08 14:31:21 -0700

    Update fpp-to-json.scala

---
+++ compiler/tools/fpp-to-json/src/main/scala/fpp-to-json.scala
@@ -0,0 +1,17 @@
+package fpp.compiler
+
+import fpp.compiler.analysis._
+import fpp.compiler.ast._
+import fpp.compiler.codegen._
+import fpp.compiler.syntax._
+import fpp.compiler.transform._
+import fpp.compiler.util._
+import scopt.OParser
+
+object FPPtoJson {
+  
+   def main(args: Array[String]) = {
+     
+   }
+  
+}
