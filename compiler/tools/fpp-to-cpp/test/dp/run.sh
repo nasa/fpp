457354edded48e9862ab42b561cfb4f2a62d5b7d
bocchino <bocchino@jpl.nasa.gov>
2023-06-20 09:56:39 -0700

    Remove old dp tests

--- compiler/tools/fpp-to-cpp/test/dp/run.sh
+++
@@ -1,8 +0,0 @@
-fprime_dir=`dirname $PWD`/fprime
-
-dp()
-{
-  run_test "-i `cat deps-comma.txt`" "-p $PWD,$fprime_dir dp" dp && \
-  diff_cpp DpTestComponent && \
-  diff_cpp DpTest_DataSerializable
-}
--- compiler/tools/fpp-to-cpp/test/dp/update-ref.sh
+++
@@ -1,8 +0,0 @@
-fprime_dir=`dirname $PWD`/fprime
-
-dp()
-{
-  update "-i `cat deps-comma.txt`" "-p $PWD,$fprime_dir dp" dp
-  move_cpp DpTestComponent
-  move_cpp DpTest_DataSerializable
-}
