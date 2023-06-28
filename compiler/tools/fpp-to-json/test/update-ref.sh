2ab4a652f59f12a00a76d4611abcb5541eab3954
Elliot Fayman <elliotfayman@gmail.com>
2023-06-27 17:57:57 -0700

    Adding Update Ref Script

---
+++ compiler/tools/fpp-to-json/test/update-ref.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+. ../../../scripts/test-utils.sh
+
+fpp_to_json=../../../bin/fpp-to-json
+
+update()
+{
+  args=$1
+  infile=$2
+  if test -n "$3"
+  then
+    outfile=$3
+  else
+    outfile=$infile
+  fi
+  echo "updating $outfile.ref.txt"
+  $fpp_to_json $args $infile.fpp 2>&1 | remove_path_prefix > $outfile.ref.txt
+}
+
+for file in `find . -name '*.ref.txt'`
+do
+  rm $file
+done
+
+update "" constants
+update "" modules
+update "" types
+update "" enums
+update "" ports
+update "" simpleComponents
+update "" specialPorts
+update "" internalPorts
+update "" commands
+update "" events
+update "" telemetry
+update "" parameters
+update "" constTypesComponents
+update "" matchedPorts
+update "" passiveComponent
+update "" queuedComponets
+update "" activeComponents
+update "" simpleTopology
+update "" importedTopologies
+update "" locationMapValidator
+
