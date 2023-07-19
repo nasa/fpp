3363c8f1cfabb5d2c002151ad149ea93ac5fc8c3
bocchino <bocchino@jpl.nasa.gov>
2023-07-19 09:43:21 -0700

    Revise Locations interface

--- compiler/lib/src/main/scala/codegen/JsonEncoder.scala
+++ compiler/lib/src/main/scala/codegen/JsonEncoder.scala
@@ -654,7 +654,7 @@ case class JsonEncoder(
   // Top level method for converting Location Map Data Structure to Json
   def printLocationsMapJson(): Json = {
     val locationsList =
-      Locations.hashMapToListOfPairs().map { case (id, location) =>
+      Locations.getMap.toList.map { case (id, location) =>
         id.toString -> location.asJson
       }
     Json.obj(locationsList: _*)
