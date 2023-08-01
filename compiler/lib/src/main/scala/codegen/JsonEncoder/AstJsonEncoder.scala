31481572e51b2e96c292ce43a06fa714f389e966
bocchino <bocchino@jpl.nasa.gov>
2023-08-01 14:01:22 -0700

    Revise Ast Json encoder

--- compiler/lib/src/main/scala/codegen/JsonEncoder/AstJsonEncoder.scala
+++ compiler/lib/src/main/scala/codegen/JsonEncoder/AstJsonEncoder.scala
@@ -21,46 +21,6 @@ object AstJsonEncoder extends JsonEncoder {
     }
 
   // ----------------------------------------------------------------------
-  // JSON encoders for Scala type variants, each of which has one value
-  // with no arguments. We use the unqualified class name of the
-  // type to represent the value.
-  //
-  // TODO: Replace all these ad-hoc transforms with a JSON-to-JSON
-  // postprocessing that replaces { "foo" : { } } with "foo" everywhere.
-  // ----------------------------------------------------------------------
-
-  implicit val binopEncoder: Encoder[Ast.Binop] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-  implicit val componentKindEncoder: Encoder[Ast.ComponentKind] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-  implicit val formalParamKindEncoder: Encoder[Ast.FormalParam.Kind] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-  implicit val generalKindEncoder: Encoder[Ast.SpecPortInstance.GeneralKind] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-  implicit val limitKindEncoder: Encoder[Ast.SpecTlmChannel.LimitKind] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-  implicit val patternEncoder: Encoder[Ast.SpecConnectionGraph.Pattern.Kind] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-  implicit val queueFullEncoder: Encoder[Ast.QueueFull] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-  implicit val severityEncoder: Encoder[Ast.SpecEvent.Severity] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-  implicit val specialInputKindEncoder: Encoder[Ast.SpecPortInstance.SpecialInputKind] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-  implicit val specialKindEncoder: Encoder[Ast.SpecPortInstance.SpecialKind] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-  implicit val typeFloatEncoder: Encoder[Ast.TypeFloat] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-  implicit val typeIntEncoder: Encoder[Ast.TypeInt] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-  implicit val unopEncoder: Encoder[Ast.Unop] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-  implicit val updateEncoder: Encoder[Ast.SpecTlmChannel.Update] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-  implicit val visibilityEncoder: Encoder[Ast.Visibility] =
-    Encoder.encodeString.contramap(getUnqualifiedClassName(_))
-
-  // ----------------------------------------------------------------------
   // Encoders for helping Circe with recursive types
   // ----------------------------------------------------------------------
 
@@ -73,21 +33,15 @@ object AstJsonEncoder extends JsonEncoder {
     io.circe.generic.semiauto.deriveEncoder[Ast.Expr]
 
   // JSON encoder for type names
-  // TODO: Replace the ad-hoc processing. See above.
   implicit val typeNameEncoder: Encoder[Ast.TypeName] =
-    Encoder.instance(
-      (t: Ast.TypeName) => t match {
-        case Ast.TypeNameBool => addTypeNameKey(t, Json.fromString("TypeNameBool"))
-        case t => io.circe.generic.semiauto.deriveEncoder[Ast.TypeName].apply(t)
-      }
-    )
+    io.circe.generic.semiauto.deriveEncoder[Ast.TypeName]
 
   // JSON encoder for module member nodes
   private implicit val moduleMemberNodeEncoder: Encoder[Ast.ModuleMember.Node] =
     io.circe.generic.semiauto.deriveEncoder[Ast.ModuleMember.Node]
 
   // ----------------------------------------------------------------------
-  // Encoders for skipping the node field
+  // Encoders for skipping the node field in member lists
   // This reduces clutter in the output
   // ----------------------------------------------------------------------
 
