2bb5c25e474059d0f3c09caae8f3488cd610b4cd
bocchino <bocchino@jpl.nasa.gov>
2023-09-11 11:02:43 -0700

    Reverse order of files in diff scripts

--- compiler/tools/fpp-to-cpp/test/scripts/run.sh
+++ compiler/tools/fpp-to-cpp/test/scripts/run.sh
@@ -42,9 +42,9 @@ diff_cpp()
   file=$1
   target_suffix=$2
   cp $file'Ac'.hpp $file'Ac'$target_suffix.out.hpp && \
-  diff -u $file'Ac'$target_suffix.out.hpp $file'Ac'$target_suffix.ref.hpp && \
+  diff -u $file'Ac'$target_suffix.ref.hpp $file'Ac'$target_suffix.out.hpp && \
   cp $file'Ac'.cpp $file'Ac'$target_suffix.out.cpp && \
-  diff -u $file'Ac'$target_suffix.out.cpp $file'Ac'$target_suffix.ref.cpp
+  diff -u $file'Ac'$target_suffix.ref.cpp $file'Ac'$target_suffix.out.cpp
 }
 
 diff_template()
@@ -52,9 +52,9 @@ diff_template()
   file=$1
   target_suffix=$2
   cp $file.template.hpp $file$target_suffix.out.template.hpp && \
-  diff -u $file$target_suffix.out.template.hpp $file$target_suffix.template.ref.hpp && \
+  diff -u $file$target_suffix.template.ref.hpp $file$target_suffix.out.template.hpp && \
   cp $file.template.cpp $file$target_suffix.out.template.cpp && \
-  diff -u $file$target_suffix.out.template.cpp $file$target_suffix.template.ref.cpp
+  diff -u $file$target_suffix.template.ref.cpp $file$target_suffix.out.template.cpp
 }
 
 diff_test()
@@ -62,13 +62,13 @@ diff_test()
   file=$1
   target_suffix=$2
   cp $file'TesterBase'.hpp $file'TesterBase'$target_suffix.out.hpp && \
-  diff -u $file'TesterBase'$target_suffix.out.hpp $file'TesterBase'$target_suffix.ref.hpp && \
+  diff -u $file'TesterBase'$target_suffix.ref.hpp $file'TesterBase'$target_suffix.out.hpp && \
   cp $file'TesterBase'.cpp $file'TesterBase'$target_suffix.out.cpp && \
-  diff -u $file'TesterBase'$target_suffix.out.cpp $file'TesterBase'$target_suffix.ref.cpp && \
+  diff -u $file'TesterBase'$target_suffix.ref.cpp $file'TesterBase'$target_suffix.out.cpp && \
   cp $file'GTestBase'.hpp $file'GTestBase'$target_suffix.out.hpp && \
-  diff -u $file'GTestBase'$target_suffix.out.hpp $file'GTestBase'$target_suffix.ref.hpp && \
+  diff -u $file'GTestBase'$target_suffix.ref.hpp $file'GTestBase'$target_suffix.out.hpp && \
   cp $file'GTestBase'.cpp $file'GTestBase'$target_suffix.out.cpp && \
-  diff -u $file'GTestBase'$target_suffix.out.cpp $file'GTestBase'$target_suffix.ref.cpp && \
+  diff -u $file'GTestBase'$target_suffix.ref.cpp $file'GTestBase'$target_suffix.out.cpp && \
   diff_cpp $file'Component'
 }
 
@@ -77,13 +77,13 @@ diff_test_template()
   file=$1
   target_suffix=$2
   cp $file'Tester'.hpp $file'Tester'$target_suffix.out.hpp && \
-  diff -u $file'Tester'$target_suffix.out.hpp $file'Tester'$target_suffix.ref.hpp && \
+  diff -u $file'Tester'$target_suffix.ref.hpp $file'Tester'$target_suffix.out.hpp && \
   cp $file'Tester'.cpp $file'Tester'$target_suffix.out.cpp && \
-  diff -u $file'Tester'$target_suffix.out.cpp $file'Tester'$target_suffix.ref.cpp && \
+  diff -u $file'Tester'$target_suffix.ref.cpp $file'Tester'$target_suffix.out.cpp && \
   cp $file'TesterHelpers'.cpp $file'TesterHelpers'$target_suffix.out.cpp && \
-  diff -u $file'TesterHelpers'$target_suffix.out.cpp $file'TesterHelpers'$target_suffix.ref.cpp && \
+  diff -u $file'TesterHelpers'$target_suffix.ref.cpp $file'TesterHelpers'$target_suffix.out.cpp && \
   cp $file'TestMain'.cpp $file'TestMain'$target_suffix.out.cpp && \
-  diff -u $file'TestMain'$target_suffix.out.cpp $file'TestMain'$target_suffix.ref.cpp
+  diff -u $file'TestMain'$target_suffix.ref.cpp $file'TestMain'$target_suffix.out.cpp
 }
 
 . ./run.sh
