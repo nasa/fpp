883a9bcc54c0b74624534b0720b258d36ef3267a
bocchino <bocchino@jpl.nasa.gov>
2023-09-21 17:07:19 -0700

    Revise component ut impl tests

--- compiler/tools/fpp-to-cpp/test/component/test-impl/update-ref.sh
+++ compiler/tools/fpp-to-cpp/test/component/test-impl/update-ref.sh
@@ -24,21 +24,27 @@ passive()
 active()
 {
   update "-u -t -i `cat ../deps-comma.txt`" "-p $PWD,$fprime_dir ../active" active
-  move_test_template ActiveTest
-  move_test_template ActiveSerial
   move_test_template ActiveCommands
   move_test_template ActiveEvents
-  move_test_template ActiveTelemetry
+  move_test_template ActiveGetProducts
+  move_test_template ActiveGuardedProducts
   move_test_template ActiveParams
+  move_test_template ActiveSerial
+  move_test_template ActiveSyncProducts
+  move_test_template ActiveTelemetry
+  move_test_template ActiveTest
 }
 
 queued()
 {
   update "-u -t -i `cat ../deps-comma.txt`" "-p $PWD,$fprime_dir ../queued" queued
-  move_test_template QueuedTest
-  move_test_template QueuedSerial
   move_test_template QueuedCommands
   move_test_template QueuedEvents
-  move_test_template QueuedTelemetry
+  move_test_template QueuedGetProducts
+  move_test_template QueuedGuardedProducts
   move_test_template QueuedParams
+  move_test_template QueuedSerial
+  move_test_template QueuedSyncProducts
+  move_test_template QueuedTelemetry
+  move_test_template QueuedTest
 }
