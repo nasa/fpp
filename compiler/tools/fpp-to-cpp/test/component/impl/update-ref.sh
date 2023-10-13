24f3385da6cf0d3e9489996e52369927837bae6a
bocchino <bocchino@jpl.nasa.gov>
2023-10-13 09:19:53 -0700

    Revise fpp-to-cpp dp tests

--- compiler/tools/fpp-to-cpp/test/component/impl/update-ref.sh
+++ compiler/tools/fpp-to-cpp/test/component/impl/update-ref.sh
@@ -12,10 +12,12 @@ passive()
   update "-t -i `cat ../deps-comma.txt`" "-p $component_dir,$fprime_dir ../passive" passive
   move_template PassiveCommands
   move_template PassiveEvents
+  move_template PassiveGetPortsOnly
   move_template PassiveGetProducts
   move_template PassiveGuardedProducts
   move_template PassiveParams
   move_template PassiveSerial
+  move_template PassiveSyncPortsOnly
   move_template PassiveSyncProducts
   move_template PassiveTelemetry
   move_template PassiveTest
