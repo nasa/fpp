1373690221a084e339f3562f3d46799bb895bc14
Rob Bocchino <bocchino@jpl.nasa.gov>
2024-07-22 08:48:52 -0700

    Add State Machine Use Analyzer

--- compiler/lib/src/main/scala/analysis/Analyzers/StateMachine/StateMachineAnalysisVisitor.scala
+++ compiler/lib/src/main/scala/analysis/Analyzers/StateMachine/StateMachineAnalysisVisitor.scala
@@ -8,10 +8,10 @@ trait StateMachineAnalysisVisitor extends AstStateVisitor {
   type State = StateMachineAnalysis
 
   /** Apply an analysis to an option value */
-  final def opt[T] (f: (StateMachineAnalysis, T) => Result) (a: StateMachineAnalysis, o: Option[T]): Result =
+  final def opt[T] (f: (StateMachineAnalysis, T) => Result) (sma: StateMachineAnalysis, o: Option[T]): Result =
     o match {
-      case Some(x) => f(a, x)
-      case None => Right(a)
+      case Some(x) => f(sma, x)
+      case None => Right(sma)
     }
 
   override def defStateMachineAnnotatedNode(
---
+++ compiler/lib/src/main/scala/analysis/Analyzers/StateMachine/StateMachineUseAnalyzer.scala
@@ -0,0 +1,56 @@
+package fpp.compiler.analysis
+
+import fpp.compiler.ast._
+import fpp.compiler.util._
+
+/** Analyze state machine uses */
+trait StateMachineUseAnalyzer
+  extends StateMachineAnalysisVisitor
+  with StateAnalyzer
+{
+
+  /** A use of a guard definition */
+  def guardUse(sma: StateMachineAnalysis, node: AstNode[Ast.Ident], use: Name.Unqualified): Result = default(sma)
+
+  /** A use of a signal definition */
+  def signalUse(sma: StateMachineAnalysis, node: AstNode[Ast.Ident], use: Name.Unqualified): Result = default(sma)
+
+  /** A use of a state definition or junction definition */
+  def stateOrJunctionUse(sma: StateMachineAnalysis, node: AstNode[Ast.QualIdent], use: Name.Qualified): Result = default(sma)
+
+  /** A use of an action definition */
+  def actionUse(sma: StateMachineAnalysis, node: AstNode[Ast.Ident], use: Name.Unqualified): Result = default(sma)
+
+  override def specInitialAnnotatedNode(sma: StateMachineAnalysis, node: Ast.Annotated[AstNode[Ast.SpecInitial]]) =
+    enterExpr(sma, node._2.data.enterExpr)
+
+  override def defJunctionAnnotatedNode(sma: StateMachineAnalysis, node: Ast.Annotated[AstNode[Ast.DefJunction]]) =
+    // TODO
+    default(sma)
+
+  override def specTransitionAnnotatedNode(sma: StateMachineAnalysis, node: Ast.Annotated[AstNode[Ast.SpecTransition]]) =
+    // TODO
+    default(sma)
+
+  private def enterExpr(sma: StateMachineAnalysis, e: Ast.EnterExpr): Result = {
+    for {
+      sma <- opt(identNode(actionUse))(sma, e.action)
+      sma <- qualIdentNode(stateOrJunctionUse)(sma, e.state)
+    }
+    yield sma
+  }
+
+  private def identNode
+    (f: (StateMachineAnalysis, AstNode[Ast.Ident], Name.Unqualified) => Result) 
+    (sma: StateMachineAnalysis, ident: AstNode[Ast.Ident]): Result = {
+    f(sma, ident, ident.data)
+  }
+
+  private def qualIdentNode
+    (f: (StateMachineAnalysis, AstNode[Ast.QualIdent], Name.Qualified) => Result) 
+    (sma: StateMachineAnalysis, qualIdent: AstNode[Ast.QualIdent]): Result = {
+    val use = Name.Qualified.fromQualIdent(qualIdent.data)
+    f(sma, qualIdent, use)
+  }
+
+}
