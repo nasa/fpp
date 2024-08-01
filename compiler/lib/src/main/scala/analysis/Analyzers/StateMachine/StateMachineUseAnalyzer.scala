37a8889ef0b096f1eb787064a7e88d593020b4ed
Rob Bocchino <bocchino@jpl.nasa.gov>
2024-08-01 09:32:21 -0700

    Revise State Machine Use Analyzer

--- compiler/lib/src/main/scala/analysis/Analyzers/StateMachine/StateMachineUseAnalyzer.scala
+++ compiler/lib/src/main/scala/analysis/Analyzers/StateMachine/StateMachineUseAnalyzer.scala
@@ -9,45 +9,106 @@ trait StateMachineUseAnalyzer
   with StateAnalyzer
 {
 
+  // ----------------------------------------------------------------------
+  // Interface methods to override
+  // Each of these methods is called when a corresponding use occurs
+  // ----------------------------------------------------------------------
+
   /** A use of an action definition */
-  def actionUse(sma: StateMachineAnalysis, node: AstNode[Ast.Ident], use: Name.Unqualified): Result = default(sma)
+  def actionUse(
+    sma: StateMachineAnalysis,
+    node: AstNode[Ast.Ident],
+    use: Name.Unqualified
+  ): Result = default(sma)
 
   /** A use of a guard definition */
-  def guardUse(sma: StateMachineAnalysis, node: AstNode[Ast.Ident], use: Name.Unqualified): Result = default(sma)
+  def guardUse(
+    sma: StateMachineAnalysis,
+    node: AstNode[Ast.Ident],
+    use: Name.Unqualified
+  ): Result = default(sma)
 
   /** A use of a signal definition */
-  def signalUse(sma: StateMachineAnalysis, node: AstNode[Ast.Ident], use: Name.Unqualified): Result = default(sma)
+  def signalUse(
+    sma: StateMachineAnalysis,
+    node: AstNode[Ast.Ident],
+    use: Name.Unqualified
+  ): Result = default(sma)
 
   /** A use of a state definition or junction definition */
-  def stateOrJunctionUse(sma: StateMachineAnalysis, node: AstNode[Ast.QualIdent], use: Name.Qualified): Result = default(sma)
+  def stateOrJunctionUse(
+    sma: StateMachineAnalysis,
+    node: AstNode[Ast.QualIdent],
+    use: Name.Qualified
+  ): Result = default(sma)
 
-  override def defJunctionAnnotatedNode(sma: StateMachineAnalysis, node: Ast.Annotated[AstNode[Ast.DefJunction]]) =
-    // TODO
-    default(sma)
+  // ----------------------------------------------------------------------
+  // Implementation using StateMachineAnalysisVisitor
+  // ----------------------------------------------------------------------
 
-  override def specInitialAnnotatedNode(sma: StateMachineAnalysis, node: Ast.Annotated[AstNode[Ast.SpecInitial]]) =
-    transitionExpr(sma, node._2.data.transition)
-
-  override def specTransitionAnnotatedNode(sma: StateMachineAnalysis, node: Ast.Annotated[AstNode[Ast.SpecTransition]]) =
-    // TODO
-    default(sma)
-
-  private def transitionExpr(sma: StateMachineAnalysis, e: Ast.TransitionExpr): Result = {
+  override def defJunctionAnnotatedNode(
+    sma: StateMachineAnalysis,
+    node: Ast.Annotated[AstNode[Ast.DefJunction]]
+  ) = {
+    val data = node._2.data
     for {
-      sma <- Result.foldLeft (e.actions) (sma) (identNode(actionUse))
-      sma <- qualIdentNode(stateOrJunctionUse)(sma, e.destination)
+      sma <- identNode(guardUse)(sma, data.guard)
+      sma <- transitionExpr(sma, data.ifTransition.data)
+      sma <- transitionExpr(sma, data.elseTransition.data)
     }
     yield sma
   }
 
-  private def identNode
-    (f: (StateMachineAnalysis, AstNode[Ast.Ident], Name.Unqualified) => Result) 
-    (sma: StateMachineAnalysis, ident: AstNode[Ast.Ident]): Result = {
-    f(sma, ident, ident.data)
+  override def specInitialAnnotatedNode(
+    sma: StateMachineAnalysis,
+    node: Ast.Annotated[AstNode[Ast.SpecInitial]]
+  ) = transitionExpr(sma, node._2.data.transition)
+
+  override def specTransitionAnnotatedNode(
+    sma: StateMachineAnalysis,
+    node: Ast.Annotated[AstNode[Ast.SpecTransition]]
+  ) = {
+    val data = node._2.data
+    for {
+      sma <- identNode(signalUse)(sma, data.signal)
+      sma <- opt(identNode(guardUse))(sma, data.guard)
+      sma <- transitionOrDo(sma, data.transitionOrDo)
+    }
+    yield sma
   }
 
+  // ----------------------------------------------------------------------
+  // Private helper methods
+  // ----------------------------------------------------------------------
+
+  private def transitionExpr(
+    sma: StateMachineAnalysis,
+    e: Ast.TransitionExpr
+  ): Result =
+    for {
+      sma <- actions(sma, e.actions)
+      sma <- qualIdentNode(stateOrJunctionUse)(sma, e.destination)
+    }
+    yield sma
+
+  private def actions(sma: StateMachineAnalysis, actions: List[AstNode[Ast.Ident]]) =
+    Result.foldLeft (actions) (sma) (identNode(actionUse))
+
+  private def transitionOrDo(
+    sma: StateMachineAnalysis,
+    tod: Ast.TransitionOrDo
+  ): Result = tod match {
+    case Ast.TransitionOrDo.Transition(e) => transitionExpr(sma, e)
+    case Ast.TransitionOrDo.Do(as) => actions(sma, as)
+  }
+
+  private def identNode
+    (f: (StateMachineAnalysis, AstNode[Ast.Ident], Name.Unqualified) => Result)
+    (sma: StateMachineAnalysis, ident: AstNode[Ast.Ident]): Result =
+    f(sma, ident, ident.data)
+
   private def qualIdentNode
-    (f: (StateMachineAnalysis, AstNode[Ast.QualIdent], Name.Qualified) => Result) 
+    (f: (StateMachineAnalysis, AstNode[Ast.QualIdent], Name.Qualified) => Result)
     (sma: StateMachineAnalysis, qualIdent: AstNode[Ast.QualIdent]): Result = {
     val use = Name.Qualified.fromQualIdent(qualIdent.data)
     f(sma, qualIdent, use)
