98f688ddcb5f09b57215d4d5575b7c4a33d29d0a
Rob Bocchino <bocchino@jpl.nasa.gov>
2024-08-01 15:58:37 -0700

    Refactor Check State Machine Uses

--- compiler/lib/src/main/scala/analysis/CheckSemantics/StateMachine/CheckStateMachineUses.scala
+++ compiler/lib/src/main/scala/analysis/CheckSemantics/StateMachine/CheckStateMachineUses.scala
@@ -6,29 +6,39 @@ import fpp.compiler.util._
 /** Match uses to their definitions */
 object CheckStateMachineUses extends StateMachineUseAnalyzer {
 
+  val helpers = CheckUsesHelpers(
+    (sma: StateMachineAnalysis) => sma.nestedScope,
+    (sma: StateMachineAnalysis, ns: StateMachineNestedScope) =>
+      sma.copy(nestedScope = ns),
+    (sma: StateMachineAnalysis) => sma.symbolScopeMap,
+    (sma: StateMachineAnalysis) => sma.useDefMap,
+    (sma: StateMachineAnalysis, udm: Map[AstNode.Id, StateMachineSymbol]) =>
+      sma.copy(useDefMap = udm)
+  )
+
   override def actionUse(
     sma: StateMachineAnalysis,
     node: AstNode[Ast.Ident],
     use: Name.Unqualified
-  ): Result = visitIdentNode (StateMachineNameGroup.Action) (sma, node)
+  ): Result = helpers.visitIdentNode (StateMachineNameGroup.Action) (sma, node)
 
   override def guardUse(
     sma: StateMachineAnalysis,
     node: AstNode[Ast.Ident],
     use: Name.Unqualified
-  ): Result = visitIdentNode (StateMachineNameGroup.Guard) (sma, node)
+  ): Result = helpers.visitIdentNode (StateMachineNameGroup.Guard) (sma, node)
 
   override def signalUse(
     sma: StateMachineAnalysis,
     node: AstNode[Ast.Ident],
     use: Name.Unqualified
-  ): Result = visitIdentNode (StateMachineNameGroup.Signal) (sma, node)
+  ): Result = helpers.visitIdentNode (StateMachineNameGroup.Signal) (sma, node)
 
   override def stateOrJunctionUse(
     sma: StateMachineAnalysis,
     node: AstNode[Ast.QualIdent],
     use: Name.Qualified
-  ): Result = visitQualIdentNode (StateMachineNameGroup.State) (sma, node)
+  ): Result = helpers.visitQualIdentNode (StateMachineNameGroup.State) (sma, node)
 
   override def defStateAnnotatedNode(
     sma: StateMachineAnalysis,
@@ -46,72 +56,5 @@ object CheckStateMachineUses extends StateMachineUseAnalyzer {
       }
     } yield sma.copy(nestedScope = sma.nestedScope.pop)
   }
-
-  private def visitIdentNode (ng: StateMachineNameGroup) (
-    sma: StateMachineAnalysis,
-    node: AstNode[Ast.Ident]
-  ) = visitUnqualifiedName (ng) (sma, node.id, node.data)
-
-  private def visitUnqualifiedName (ng: StateMachineNameGroup) (
-    sma: StateMachineAnalysis,
-    id: AstNode.Id,
-    name: Ast.Ident
-  ) = {
-    val mapping = sma.nestedScope.get (ng) _
-    for (symbol <- getSymbolForName(mapping)(id, name)) yield {
-      val useDefMap = sma.useDefMap + (id -> symbol)
-      sma.copy(useDefMap = useDefMap)
-    }
-  }
-
-  private def getSymbolForName
-    (mapping: Name.Unqualified => Option[StateMachineSymbol]) 
-    (id: AstNode.Id, name: Name.Unqualified): Result.Result[StateMachineSymbol] =
-    mapping(name).map(Right(_)).getOrElse({
-      val loc = Locations.get(id)
-      Left(SemanticError.UndefinedSymbol(name, loc))
-    })
-
-  private def visitQualIdentNode (ng: StateMachineNameGroup) (
-    sma: StateMachineAnalysis,
-    node: AstNode[Ast.QualIdent]
-  ): Result =
-    node.data match {
-      case Ast.QualIdent.Unqualified(name) =>
-        visitUnqualifiedName (ng) (sma, node.id, name)
-      case Ast.QualIdent.Qualified(qualifier, name) =>
-        visitQualifiedName (ng) (sma, node.id, qualifier, name)
-    }
-
-  private def visitQualifiedName (ng: StateMachineNameGroup) (
-    sma: StateMachineAnalysis,
-    id: AstNode.Id,
-    qualifier: AstNode[Ast.QualIdent],
-    name: AstNode[Ast.Ident]
-  ) =
-    for {
-      sma <- visitQualIdentNode (ng) (sma, qualifier)
-      scope <- {
-        val symbol = sma.useDefMap(qualifier.id)
-        sma.symbolScopeMap.get(symbol).map(Right(_)).getOrElse(
-          Left(
-            SemanticError.InvalidSymbol(
-              symbol.getUnqualifiedName,
-              Locations.get(id),
-              "not a qualifier",
-              symbol.getLoc
-            )
-          )
-        )
-      }
-      symbol <- {
-        val mapping = scope.get (ng) _
-        getSymbolForName(mapping)(name.id, name.data)
-      }
-    }
-    yield {
-      val useDefMap = sma.useDefMap + (id -> symbol)
-      sma.copy(useDefMap = useDefMap)
-    }
 
 }
