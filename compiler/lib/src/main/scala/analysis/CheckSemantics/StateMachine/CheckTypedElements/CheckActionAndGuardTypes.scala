33475cdaa24e511297ceaca9290ccfd293986eb5
Rob Bocchino <bocchino@jpl.nasa.gov>
2024-09-08 19:16:22 -0700

    Refactor Check Action and Guard Types

--- compiler/lib/src/main/scala/analysis/CheckSemantics/StateMachine/CheckTypedElements/CheckActionAndGuardTypes.scala
+++ compiler/lib/src/main/scala/analysis/CheckSemantics/StateMachine/CheckTypedElements/CheckActionAndGuardTypes.scala
@@ -13,7 +13,7 @@ object CheckActionAndGuardTypes
     te: StateMachineTypedElement.InitialTransition
   ): Result = {
     val actions = te.aNode._2.data.transition.data.actions
-    checkActions(sma, te, actions)
+    checkActionTypes(sma, te, actions)
   }
 
   override def junctionTypedElement(
@@ -44,19 +44,49 @@ object CheckActionAndGuardTypes
     // TODO
     default(sma)
 
-  private def checkActions(
+  // Check action types
+  private def checkActionTypes(
     sma: StateMachineAnalysis,
     te: StateMachineTypedElement,
     actions: List[AstNode[Ast.Ident]]
   ): Result.Result[StateMachineAnalysis] = {
+    val siteKind = "action"
+    def getTypeOption(sym: StateMachineSymbol): Option[Type] = {
+      val actionSym @ StateMachineSymbol.Action(_) = sym
+      actionSym.node._2.data.typeName.map(tn => sma.a.typeMap(tn.id))
+    }
+    checkCallSiteTypes(sma, te, actions, siteKind, getTypeOption)
+  }
+
+  // Check guard types
+  private def checkGuardTypes(
+    sma: StateMachineAnalysis,
+    te: StateMachineTypedElement,
+    guards: List[AstNode[Ast.Ident]]
+  ): Result.Result[StateMachineAnalysis] = {
+    val siteKind = "guard"
+    def getTypeOption(sym: StateMachineSymbol): Option[Type] = {
+      val guardSym @ StateMachineSymbol.Guard(_) = sym
+      guardSym.node._2.data.typeName.map(tn => sma.a.typeMap(tn.id))
+    }
+    checkCallSiteTypes(sma, te, guards, siteKind, getTypeOption)
+  }
+
+  // Check call site types
+  private def checkCallSiteTypes(
+    sma: StateMachineAnalysis,
+    te: StateMachineTypedElement,
+    callSites: List[AstNode[Ast.Ident]],
+    siteKind: String,
+    getTypeOption: (StateMachineSymbol => Option[Type])
+  ): Result.Result[StateMachineAnalysis] = {
     val teKind = te.showKind
     val teTo = sma.typeOptionMap(te)
-    val siteKind = "action"
-    Result.foldLeft (actions) (sma) (
-      (sma, a) => {
-        val loc = Locations.get(a.id)
-        val sym @ StateMachineSymbol.Action(_) = sma.useDefMap(a.id)
-        val siteTo = sym.node._2.data.typeName.map(tn => sma.a.typeMap(tn.id))
+    Result.foldLeft (callSites) (sma) (
+      (sma, cs) => {
+        val loc = Locations.get(cs.id)
+        val sym = sma.useDefMap(cs.id)
+        val siteTo = getTypeOption(sym)
         for {
           _ <- sma.convertTypeOptionsAtCallSite(
                  loc,
