bee1ee5709bc3cfea6998f364c0649c0812c7abc
Rob Bocchino <bocchino@jpl.nasa.gov>
2024-09-10 14:10:45 -0500

    Revise typed element tests

--- compiler/tools/fpp-check/test/state_machine/typed_elements/tests.sh
+++ compiler/tools/fpp-check/test/state_machine/typed_elements/tests.sh
@@ -5,6 +5,8 @@ junction_u32_bool_transitive
 junction_u32_none
 sm_initial_bad_action_type
 sm_initial_bad_guard_type
+sm_junction_bad_else_action_type
+sm_junction_bad_if_action_type
 state_initial_bad_action_type
 state_initial_bad_guard_type
 "
