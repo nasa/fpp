d9ba6f3c067f7fe688fdfd824ab50ab12ba3ffcb
Rob Bocchino <bocchino@jpl.nasa.gov>
2024-09-13 16:00:44 -0700

    Revise state machine spec
    Add Transition.scala

---
+++ compiler/lib/src/main/scala/analysis/Semantics/StateMachine/Transition.scala
@@ -0,0 +1,25 @@
+package fpp.compiler.analysis
+
+import fpp.compiler.ast._
+import fpp.compiler.util._
+
+/** An FPP state machine transition */
+sealed trait Transition {
+  def getActions: List[StateMachineSymbol.Action]
+}
+
+object Transition {
+
+  final case class External(
+    actions: List[StateMachineSymbol.Action],
+    destination: StateOrJunction
+  ) extends Transition {
+    def getActions = actions
+  }
+
+  final case class Internal(actions: List[StateMachineSymbol.Action])
+    extends Transition {
+      def getActions = actions
+    }
+
+}
