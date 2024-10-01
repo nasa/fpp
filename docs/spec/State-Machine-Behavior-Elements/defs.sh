04f37768aea1be4d5d3468046011a594c38c4c75
Rob Bocchino <bocchino@jpl.nasa.gov>
2024-10-01 09:00:19 -0700

    Update spec and version number
    * Update spec to latest on state machines
    * Update version number to v2.2.0

--- docs/spec/State-Machine-Behavior-Elements/defs.sh
+++ docs/spec/State-Machine-Behavior-Elements/defs.sh
@@ -9,11 +9,14 @@ redo-ifchange defs.sh
 export FILES="
 Introduction.adoc
 Action-Definitions.adoc
-Enter-Expressions.adoc
+Do-Expressions.adoc
 Guard-Definitions.adoc
 Initial-Transition-Specifiers.adoc
 Junction-Definitions.adoc
 Signal-Definitions.adoc
 State-Definitions.adoc
+State-Entry-Specifiers.adoc
+State-Exit-Specifiers.adoc
 State-Transition-Specifiers.adoc
+Transition-Expressions.adoc
 "
