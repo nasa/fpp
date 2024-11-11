198c0c6f0f1b8d2180c0d2833a27c5f6fd37c8c1
jawest <justinewest14@gmail.com>
2024-11-11 13:06:04 -0800

    update tests

--- compiler/tools/fpp-to-layout/test/top/update-ref.sh
+++ compiler/tools/fpp-to-layout/test/top/update-ref.sh
@@ -1,3 +1,9 @@
+direct()
+{
+  update "" direct
+  move_layout_files TDirect C
+}
+
 multiple_tops()
 {
   update "" multiple_tops
@@ -9,10 +15,4 @@ pattern()
 {
   update "" pattern
   move_layout_files TPattern Health
-}
-
-direct()
-{
-  update "" direct
-  move_layout_files TDirect C
 }
