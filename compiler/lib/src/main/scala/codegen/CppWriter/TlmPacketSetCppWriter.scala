47a10bef160695cceafbcd30e0b09de078884761
Rob Bocchino <bocchino@jpl.nasa.gov>
2025-02-24 20:37:48 -0800

    Revise code gen for tlm packets

--- compiler/lib/src/main/scala/codegen/CppWriter/TlmPacketSetCppWriter.scala
+++ compiler/lib/src/main/scala/codegen/CppWriter/TlmPacketSetCppWriter.scala
@@ -15,23 +15,19 @@ case class TlmPacketSetCppWriter(
 
   private val name = data.name
 
-  private val fileName = ComputeCppFiles.FileNames.getTlmPacketSet(name)
-
   private val Some(t) = s.a.topology
 
-  private val Some(d) = s.a.dictionary
+  private val d = s.a.dictionaryMap(Symbol.Topology(t.aNode))
+
+  private val tps = d.tlmPacketSetMap(name)
+
+  private val packetList = tps.packetMap.values.toList.sortBy(_.getName)
 
   private val topQualifiedName = s"${t.getName}_$name"
 
-  private val qualifiedName =
-    s.a.getEnclosingNames(Symbol.Topology(t.aNode)) match {
-      case Nil => topQualifiedName
-      case names =>
-        val qualifier = CppWriterState.identFromQualifiedName(
-          Name.Qualified.fromIdentList(names)
-        )
-        s"${qualifier}_${topQualifiedName}"
-    }
+  private val namespaceName = s"${topQualifiedName}Packets"
+
+  private val fileName = ComputeCppFiles.FileNames.getTlmPacketSet(topQualifiedName)
 
   def write: CppDoc = {
     CppWriter.createCppDoc(
@@ -43,15 +39,33 @@ case class TlmPacketSetCppWriter(
     )
   }
 
-  private def getCppChannelArraysMember: CppDoc.Member = {
+  private def getAnonymousNamespaceMember: CppDoc.Member =
+    linesMember(
+      wrapInAnonymousNamespace(
+        List.concat(
+          getCppChannelArraysLines,
+          getCppPacketsLines
+        )
+      ),
+      CppDoc.Lines.Cpp
+    )
+
+  private def getCppChannelArraysLines: List[Line] = {
     // TODO
-    linesMember(Nil)
+    Nil
   }
 
   private def getCppIncludesMember: CppDoc.Member = {
     // TODO
     linesMember(Nil)
   }
+
+  private def getCppMembers: List[CppDoc.Member] =
+    List(
+      getAnonymousNamespaceMember,
+      getCppPacketListMember,
+      getCppOmittedMember
+    )
 
   private def getCppOmittedMember: CppDoc.Member = {
     // TODO
@@ -63,52 +77,96 @@ case class TlmPacketSetCppWriter(
     linesMember(Nil)
   }
 
-  private def getCppPacketsMember: CppDoc.Member = {
+  private def getCppPacketsLines: List[Line] = {
     // TODO
-    linesMember(Nil)
+    Nil
   }
+
+  private def getHppConstantMembers: List[CppDoc.Member] =
+    List(
+      linesMember(
+        List.concat(
+          CppDocWriter.writeBannerComment("Constants"),
+          addBlankPrefix(
+            line("//! The packet sizes") ::
+            wrapInEnumClass(
+              "PacketSize",
+              packetList.flatMap(writePacketSizeEnum),
+              Some("FwSizeType")
+            )
+          )
+        )
+      )
+    )
 
   private def getHppIncludesMember: CppDoc.Member = {
     val headers = List(
+      "Fw/Types/StringBase.hpp",
       "Svc/TlmPacketizer/TlmPacketizerTypes.hpp"
     ).map(CppWriter.headerString)
     linesMember(addBlankPrefix(headers.sorted.map(line)))
   }
 
-  private def getHppOmittedMember: CppDoc.Member = {
-    linesMember(
-      lines(
-        s"""|
-            |//! The omitted channels
-            |extern const Svc::TlmPacketizerPacket OmittedChannels;"""
+  private def getHppMembers: List[CppDoc.Member] =
+    List.concat(
+      getHppConstantMembers,
+      getHppVarMembers
+    )
+
+  private def getHppVarMembers: List[CppDoc.Member] =
+    List(
+      linesMember(
+        List.concat(
+          CppDocWriter.writeBannerComment("Extern variables"),
+          lines(
+            s"""|
+                |//! The list of packets
+                |extern const Svc::TlmPacketizerPacketList packetList;
+                |
+                |//! The omitted channels
+                |extern const Svc::TlmPacketizerPacket omittedChannels;"""
+          )
+        )
       )
     )
-  }
 
-  private def getHppPacketListMember: CppDoc.Member = {
-    linesMember(
-      lines(
-        s"""|
-            |//! The list of packets
-            |extern const Svc::TlmPacketizerPacketList PacketList;"""
-      )
-    )
+  private def getIncludeGuard: String = {
+    val guard = s.a.getEnclosingNames(Symbol.Topology(t.aNode)) match {
+      case Nil => namespaceName
+      case names =>
+        val qualifier = CppWriterState.identFromQualifiedName(
+          Name.Qualified.fromIdentList(names)
+        )
+        s"${qualifier}_${namespaceName}"
+    }
+    s"${guard}_HPP"
   }
-
-  private def getIncludeGuard: String = s"${qualifiedName}_HPP"
 
   private def getMembers: List[CppDoc.Member] = {
-    val nsil = s.getNamespaceIdentList(Symbol.Topology(t.aNode)) :+ topQualifiedName
-    val varMembers = List(
-      getCppChannelArraysMember,
-      getCppPacketsMember,
-      getHppPacketListMember,
-      getCppPacketListMember,
-      getHppOmittedMember,
-      getCppOmittedMember
-    )
+    val nsil = s.getNamespaceIdentList(Symbol.Topology(t.aNode)) :+
+      namespaceName
+    val members = getHppMembers ++ getCppMembers
     getHppIncludesMember :: getCppIncludesMember ::
-      wrapInNamespaces(nsil, varMembers)
+      wrapInNamespaces(nsil, members)
+  }
+
+  private def writeChannelSize(id: TlmChannel.Id) = {
+    val entry = d.tlmChannelEntryMap(id)
+    entry.tlmChannel.channelType match {
+      case (ts: Type.String) =>
+        val stringSize = writeStringSize(s, ts)
+        s"Fw::StringBase::STATIC_SERIALIZED_SIZE(FW_MIN($stringSize, FW_TLM_STRING_MAX_SIZE))"
+      case t =>
+        val tn = TypeCppWriter.getName(s, t)
+        writeSerializedSizeExpr(s, t, tn)
+    }
+  }
+
+  private def writePacketSizeEnum(tp: TlmPacket): List[Line] = {
+    val name = tp.getName
+    val idList = tp.memberIdList
+    val channelSize = idList.map(writeChannelSize).mkString("\n  + ")
+    lines(s"$name = $channelSize,")
   }
 
 }
