ac4fc428781f36c047029d65fdf6aed42477d3a0
Rob Bocchino <bocchino@jpl.nasa.gov>
2025-06-12 12:34:03 -0700

    Revise checking for framework types

--- compiler/lib/src/main/scala/analysis/CheckSemantics/CheckFrameworkDefs.scala
+++ compiler/lib/src/main/scala/analysis/CheckSemantics/CheckFrameworkDefs.scala
@@ -13,11 +13,25 @@ object CheckFrameworkDefs
     val name = a.getQualifiedName (Symbol.AliasType(aNode)).toString
     val id = aNode._2.id
     name match {
+      case "FwAssertArgType" => requireIntegerTypeAlias(a, name, id)
       case "FwChanIdType" => requireIntegerTypeAlias(a, name, id)
+      case "FwDpIdType" => requireIntegerTypeAlias(a, name, id)
+      case "FwDpPriorityType" => requireIntegerTypeAlias(a, name, id)
+      case "FwEnumStoreType" => requireIntegerTypeAlias(a, name, id)
       case "FwEventIdType" => requireIntegerTypeAlias(a, name, id)
+      case "FwIndexType" => requireSignedIntegerTypeAlias(a, name, id)
       case "FwOpcodeType" => requireIntegerTypeAlias(a, name, id)
       case "FwPacketDescriptorType" => requireIntegerTypeAlias(a, name, id)
+      case "FwPrmIdType" => requireIntegerTypeAlias(a, name, id)
+      case "FwQueuePriorityType" => requireIntegerTypeAlias(a, name, id)
+      case "FwSignedSizeType" => requireSignedIntegerTypeAlias(a, name, id)
+      case "FwSizeStoreType" => requireIntegerTypeAlias(a, name, id)
+      case "FwSizeType" => requireUnsignedIntegerTypeAlias(a, name, id)
+      case "FwPriorityType" => requireIntegerTypeAlias(a, name, id)
+      case "FwTimeBaseStoreType" => requireIntegerTypeAlias(a, name, id)
+      case "FwTimeContextStoreType" => requireIntegerTypeAlias(a, name, id)
       case "FwTlmPacketizeIdType" => requireIntegerTypeAlias(a, name, id)
+      case "FwTraceIdType" => requireIntegerTypeAlias(a, name, id)
       case _ => Right(a)
     }
   }
@@ -31,5 +45,31 @@ object CheckFrameworkDefs
         s"the F Prime framework type ${name} must be an alias of an integer type"
       )
     )
+
+  private def requireSignedIntegerTypeAlias(a: Analysis, name: String, id: AstNode.Id) = {
+    val valid = a.typeMap(id).getUnderlyingType match {
+      case pi: Type.PrimitiveInt => pi.signedness == Type.PrimitiveInt.Signed
+      case _ => false
+    }
+    if (valid) then Right(a) else Left(
+      SemanticError.InvalidType(
+        Locations.get(a.useDefMap(id).getNodeId),
+        s"the F Prime framework type ${name} must be an alias of a signed integer type"
+      )
+    )
+  }
+
+  private def requireUnsignedIntegerTypeAlias(a: Analysis, name: String, id: AstNode.Id) = {
+    val valid = a.typeMap(id).getUnderlyingType match {
+      case pi: Type.PrimitiveInt => pi.signedness == Type.PrimitiveInt.Unsigned
+      case _ => false
+    }
+    if (valid) then Right(a) else Left(
+      SemanticError.InvalidType(
+        Locations.get(a.useDefMap(id).getNodeId),
+        s"the F Prime framework type ${name} must be an alias of an unsigned integer type"
+      )
+    )
+  }
 
 }
