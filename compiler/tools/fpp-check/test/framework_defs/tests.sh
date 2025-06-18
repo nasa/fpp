71cb8f31128ce1cd127d35754b0b49e2f256efc2
jawest <justinewest14@gmail.com>
2025-06-18 11:01:06 -0700

    Update tests

--- compiler/tools/fpp-check/test/framework_defs/tests.sh
+++ compiler/tools/fpp-check/test/framework_defs/tests.sh
@@ -3,6 +3,7 @@ dp_state_not_enum
 fw_assert_arg_type_not_integer
 fw_event_id_type_not_alias_type
 fw_index_type_not_signed
+fw_opcode_type_not_alias_type
 fw_size_type_not_unsigned
 user_data_size_not_integer
 "
