3b1611ccbcd1cd2b5260f09cc563b230bf019ddd
Rob Bocchino <bocchino@jpl.nasa.gov>
2025-06-26 10:57:28 -0700

    Revise id range checking

--- compiler/lib/src/main/scala/analysis/CheckSemantics/CheckComponentInstanceDefs.scala
+++ compiler/lib/src/main/scala/analysis/CheckSemantics/CheckComponentInstanceDefs.scala
@@ -49,28 +49,33 @@ object CheckComponentInstanceDefs
     def check(instances: List[ComponentInstance]): Result.Result[Unit] =
       instances match {
         case (i1 :: i2 :: tail) =>
-          // case where i2 has a non-empty range of identifiers, 
-          // check that there are no base id conflicts
           if (i2.maxId >= i2.baseId && i1.baseId >= i2.baseId) 
-            return Left(
+            // Error: i2 has a nonempty ID range, and
+            // base id for i1 lies in the ID range of i2
+            Left(
               SemanticError.OverlappingIdRanges(
                 i1.baseId,
                 i1.aNode._2.data.name,
                 Locations.get(i1.aNode._2.id),
                 i2.baseId,
+                i2.maxId,
                 i2.aNode._2.data.name,
                 Locations.get(i2.aNode._2.id)
               )
             )
-          if (i1.maxId < i2.baseId) check(i2 :: tail)
+          else if (i1.maxId < i2.baseId)
+            // OK: ID ranges do not overlap; continue checking
+            check(i2 :: tail)
           else Left(
+            // Error: Base id for i2 lies in the ID range of i1
             SemanticError.OverlappingIdRanges(
-              i1.maxId,
-              i1.aNode._2.data.name,
-              Locations.get(i1.aNode._2.id),
               i2.baseId,
               i2.aNode._2.data.name,
-              Locations.get(i2.aNode._2.id)
+              Locations.get(i1.aNode._2.id),
+              i1.baseId,
+              i1.maxId,
+              i1.aNode._2.data.name,
+              Locations.get(i1.aNode._2.id)
             )
           )
         case _ => Right(())
