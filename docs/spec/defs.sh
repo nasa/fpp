75ad1fed555582482df08f328ba20900c9a0d902
Andrei Tumbar <andrei.tumbar@jpl.nasa.gov>
2025-07-08 10:19:51 -0700

    Update spec

--- docs/spec/defs.sh
+++ docs/spec/defs.sh
@@ -18,6 +18,7 @@ Definitions/Definitions.adoc
 State-Machine-Behavior-Elements/State-Machine-Behavior-Elements.adoc
 Specifiers/Specifiers.adoc
 Component-Instance-Member-Identifiers.adoc
+Endpoint-Member-Identifiers.adoc
 Type-Names.adoc
 Expressions/Expressions.adoc
 Formal-Parameter-Lists.adoc
