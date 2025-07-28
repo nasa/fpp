8011f95ccc74273333eb6ea1764bfea5a0a4c608
Andrei Tumbar <andrei.tumbar@jpl.nasa.gov>
2025-07-27 21:31:14 -0700

    Module templates

--- docs/spec/Definitions/defs.sh
+++ docs/spec/Definitions/defs.sh
@@ -17,11 +17,11 @@ Constant-Definitions.adoc
 Enum-Definitions.adoc
 Enumerated-Constant-Definitions.adoc
 Module-Definitions.adoc
+Module-Template-Definitions.adoc
 Port-Definitions.adoc
 Port-Interface-Definitions.adoc
 State-Machine-Definitions.adoc
 Struct-Definitions.adoc
 Topology-Definitions.adoc
-Template-Definitions.adoc
 Framework-Definitions.adoc
 "
