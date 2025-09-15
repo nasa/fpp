e4fa10d7e7272369ee180bd9127dc865ba9cca19
Andrei Tumbar <andrei.tumbar@jpl.nasa.gov>
2025-09-15 15:12:31 -0700

    Update compiler/tools/fpp-to-xml/src/main/scala/fpp-to-xml.scala
    Co-authored-by: Rob Bocchino <bocchino@jpl.nasa.gov>

--- compiler/tools/fpp-to-xml/src/main/scala/fpp-to-xml.scala
+++ compiler/tools/fpp-to-xml/src/main/scala/fpp-to-xml.scala
@@ -3,8 +3,7 @@ package fpp.compiler.tools
 object FPPToXml {
 
   def main(args: Array[String]) = {
-    println("fpp-to-xml is deprecated")
-    System.exit(0)
+    println("fpp-to-xml is not supported")
   }
 
 }
