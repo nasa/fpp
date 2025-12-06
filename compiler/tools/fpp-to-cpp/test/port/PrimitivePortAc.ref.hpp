28dc03171d4b180b21d1367471b5ad272e30a4a1
Rob Bocchino <bocchino@jpl.nasa.gov>
2025-12-05 17:00:59 -0800

    Fix code gen for ports

--- compiler/tools/fpp-to-cpp/test/port/PrimitivePortAc.ref.hpp
+++ compiler/tools/fpp-to-cpp/test/port/PrimitivePortAc.ref.hpp
@@ -8,12 +8,12 @@
 #define PrimitivePortAc_HPP
 
 #include "Fw/FPrimeBasicTypes.hpp"
+#include "Fw/Types/String.hpp"
 #if !FW_DIRECT_PORT_CALLS
 #include "Fw/Comp/PassiveComponentBase.hpp"
 #include "Fw/Port/InputPortBase.hpp"
 #include "Fw/Port/OutputPortBase.hpp"
 #include "Fw/Types/Serializable.hpp"
-#include "Fw/Types/String.hpp"
 #endif
 
 //! Primitive port constants
