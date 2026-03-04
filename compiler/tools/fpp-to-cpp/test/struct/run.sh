1ae59c71345800a8474cb839702dbff6ad4584e4
Rob Bocchino <bocchino@jpl.nasa.gov>
2026-03-04 13:43:35 -0800

    Refactor cpp tests

--- compiler/tools/fpp-to-cpp/test/struct/run.sh
+++ compiler/tools/fpp-to-cpp/test/struct/run.sh
@@ -1,12 +1,14 @@
+. ./fpp-options.sh
+
 abs_type()
 {
-  run_test "-p $PWD" abs_type && \
+  run_test "$fpp_options" abs_type && \
     diff_cpp AbsTypeSerializable
 }
 
 alias_type()
 {
-  run_test "-p $PWD" alias_type && \
+  run_test "$fpp_options" alias_type && \
     diff_cpp AliasTypeSerializable && \
     diff_h_hpp U16AliasAlias && \
     diff_hpp TAliasAlias && \
@@ -15,52 +17,52 @@ alias_type()
 
 array()
 {
-  run_test "-p $PWD" array && \
+  run_test "$fpp_options" array && \
     diff_cpp AArray
 }
 
 component()
 {
-  run_test "-p $PWD" component && \
+  run_test "$fpp_options" component && \
     diff_cpp C_SSerializable && \
     diff_cpp SSerializable
 }
 
 default()
 {
-  run_test "-p $PWD" default && \
+  run_test "$fpp_options" default && \
     diff_cpp DefaultSerializable
 }
 
 empty()
 {
-  run_test "-p $PWD" empty && \
+  run_test "$fpp_options" empty && \
     diff_cpp EmptySerializable
 }
 
 enum()
 {
-  run_test "-p $PWD" enum && \
+  run_test "$fpp_options" enum && \
     diff_cpp EEnum && \
     diff_cpp EnumSerializable 
 }
 
 format()
 {
-  run_test "-p $PWD" format && \
+  run_test "$fpp_options" format && \
     diff_cpp FormatSerializable
 }
 
 include()
 {
-  run_test "-p $PWD" include && \
+  run_test "$fpp_options" include && \
     diff_cpp IncludedSerializable && \
     diff_cpp IncludingSerializable
 }
 
 modules()
 {
-  run_test "-p $PWD" modules && \
+  run_test "$fpp_options" modules && \
     diff_cpp Modules1Serializable && \
     diff_cpp Modules2Serializable && \
     diff_cpp Modules3Serializable && \
@@ -69,20 +71,20 @@ modules()
 
 primitive()
 {
-  run_test "-p $PWD" primitive && \
+  run_test "$fpp_options" primitive && \
     diff_cpp PrimitiveSerializable && \
     diff_cpp PrimitiveStructSerializable
 }
 
 state_machine()
 {
-  run_test "-p $PWD" state_machine && \
+  run_test "$fpp_options" state_machine && \
     diff_cpp SM_SSerializable
 }
 
 string()
 {
-  run_test "-p $PWD" string && \
+  run_test "$fpp_options" string && \
     diff_cpp StringSerializable && \
     diff_cpp StringArraySerializable
 }
