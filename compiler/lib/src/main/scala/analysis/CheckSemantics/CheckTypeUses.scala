cbee385616cd049c6b86d4abae456f334a4c07d1
jawest <justinewest14@gmail.com>
2026-03-04 16:14:55 -0800

    Revert change

--- compiler/lib/src/main/scala/analysis/CheckSemantics/CheckTypeUses.scala
+++ compiler/lib/src/main/scala/analysis/CheckSemantics/CheckTypeUses.scala
@@ -111,7 +111,7 @@ object CheckTypeUses extends UseAnalyzer {
     visitIfNeeded(visitor)(a, aNode)
   }
 
-  override def exprNode(a: Analysis, node: AstNode[Ast.Expr]) = default(a)
+  override def exprNode(a: Analysis, node: AstNode[Ast.Expr]) = matchExprNode(a, node)
 
   override def typeNameBoolNode(a: Analysis, node: AstNode[Ast.TypeName]) =
     Right(a.assignType(node -> Type.Boolean))
