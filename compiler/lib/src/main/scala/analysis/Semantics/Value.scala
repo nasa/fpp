d936ef8d740a807462dc4229e11e92569c155dd4
abh80 <abhb787@outlook.com>
2026-04-24 13:33:02 +0530

    Added shift-amount validation predicates on Value

--- compiler/lib/src/main/scala/analysis/Semantics/Value.scala
+++ compiler/lib/src/main/scala/analysis/Semantics/Value.scala
@@ -99,6 +99,11 @@ sealed trait Value {
   /** Truncate the value based on the width of its type */
   def truncate: Value = this
 
+  /** Check if the Value is negative for the purpose of shifting operations */
+  def isNegative: Boolean = false
+
+  /** Check if the value is shiftable for the purpose of shiting operations */
+  def isValidShiftAmount: Boolean = false
 }
 
 object Value {
@@ -159,6 +164,9 @@ object Value {
 
     override def unary_- = Some(PrimitiveInt(-value, kind))
 
+    override def isNegative = (value < 0)
+
+    override def isValidShiftAmount = !isNegative && value.isValidInt
   }
 
   /** Integer values */
@@ -200,6 +208,9 @@ object Value {
 
     override def unary_- = Some(Integer(-value))
 
+    override def isNegative = (value < 0)
+    
+    override def isValidShiftAmount = !isNegative && value.isValidInt
   }
 
   /** Floating-point values */
@@ -381,6 +392,9 @@ object Value {
 
     override def unary_- = - convertToRepType
 
+    override def isNegative = convertToRepType.isNegative
+
+    override def isValidShiftAmount: scala.Boolean = convertToRepType.isValidShiftAmount
   }
 
   /** Anonymous struct values */
