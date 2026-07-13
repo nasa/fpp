3ba0ab6b5dae452f1f4078a7d1f0a9449d386ccd
Rob Bocchino <bocchino@jpl.nasa.gov>
2026-07-13 09:29:04 -0700

    Revise FppWriter and tests

--- compiler/lib/src/main/scala/codegen/FppWriter.scala
+++ compiler/lib/src/main/scala/codegen/FppWriter.scala
@@ -92,12 +92,6 @@ object FppWriter extends AstVisitor with LineUtils {
     annotate(a1, l, a2)
   }
 
-  // def systemMember(member: Ast.SystemMember): Out = {
-  //   val (a1, _, a2) = member.node
-  //   val l = matchSystemMember((), member)
-  //   annotate(a1, l, a2)
-  // }
-
   def tlmPacketSetMember(member: Ast.TlmPacketSetMember): Out = {
     val (a1, _, a2) = member.node
     val l = matchTlmPacketSetMember((), member)
@@ -334,17 +328,14 @@ object FppWriter extends AstVisitor with LineUtils {
     joinOpt (data.default) (" default ") (exprNode)
   }
 
-  // override def defSystemAnnotatedNode(
-  //   in: In,
-  //   aNode: Ast.Annotated[AstNode[Ast.DefSystem]]
-  // ) = {
-  //   val (_, node, _) = aNode
-  //   val data = node.data
-  //   lines(s"system ${ident(data.name)} {") ++
-  //     List(Line.blank) ++
-  //     Line.blankSeparated (systemMember) (data.members).map(indentIn) ++
-  //     List(Line.blank, line("}"))
-  // }
+  override def defSystemAnnotatedNode(
+    in: In,
+    aNode: Ast.Annotated[AstNode[Ast.DefSystem]]
+  ) = {
+    val (_, node, _) = aNode
+    val data = node.data
+    lines(s"system ${ident(data.name)}:").join(" ")(qualIdent(data.topology.data))
+  }
 
   override def defTopologyAnnotatedNode(
     in: In,
@@ -504,15 +495,6 @@ object FppWriter extends AstVisitor with LineUtils {
       joinOpt (data.id) (" id ") (exprNode).
       joinOpt (data.defaultPriority) (" default priority ") (exprNode)
   }
-
-  // override def specDeploymentAnnotatedNode(
-  //   in: In,
-  //   aNode: Ast.Annotated[AstNode[Ast.SpecDeployment]]
-  // ) = {
-  //   val (_, node, _) = aNode
-  //   val data = node.data
-  //   lines("deployment").join (" ") (qualIdent(data.topology.data))
-  // }
 
   override def specEventAnnotatedNode(
     in: In,
