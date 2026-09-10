a00c51733c2b0b1db9bb8d8f9b8e224be8a809dc
Andrei Tumbar <andreitumbar@gmail.com>
2026-09-09 18:36:39 -0700

    Fix duplicate AddStateEnums

--- compiler/tools/fpp/src/main/scala/fpp-filenames.scala
+++ compiler/tools/fpp/src/main/scala/fpp-filenames.scala
@@ -24,7 +24,6 @@ object FPPFilenames {
     }
     for {
       tul <- ToolUtils.parseFilesAndResolveAsts(Analysis(), files).map(_._2)
-      tul <- AddStateEnums.transUnitList(tul)
       files <-
         CppWriter.getMode(options.template, options.unitTest) match {
           case CppWriter.Autocode => ComputeGeneratedFiles.getAutocodeFiles(tul)
--- compiler/tools/fpp/src/main/scala/fpp-locate-defs.scala
+++ compiler/tools/fpp/src/main/scala/fpp-locate-defs.scala
@@ -27,7 +27,6 @@ object FPPLocateDefs {
       aTul <- ToolUtils.parseFilesAndResolveAsts(Analysis(), files)
       a <- Right(aTul._1)
       tul <- Right(aTul._2)
-      tul <- AddStateEnums.transUnitList(tul)
       tul <- expandTemplates(a, tul)
     }
     yield {
